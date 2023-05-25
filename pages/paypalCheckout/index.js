48f5de2f8ef1bd1f0ea8d4b3b84ebd12e236ca99
Joakim <joakim@spprod.se>
2023-05-25 14:37:07 +0200

    Change paypalcheckout index name to 'Index'

--- pages/paypalCheckout/index.js
+++ pages/paypalCheckout/index.js
@@ -2,7 +2,7 @@ import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js"
 import prisma from "@/components/prisma";
 import { useRouter } from "next/router";
 
-export default function index({ photosInCart, email }) {
+export default function Index({ photosInCart, email }) {
 const router = useRouter()
 
   const sumOfCart = photosInCart.reduce((total, photo) => {
