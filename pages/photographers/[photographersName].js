5cceccbeaf2abef6c1a224260c786adbe8678bbf
Joakim <joakim@spprod.se>
2023-08-01 16:21:29 +0200

    Update photographers pages. (rough design)

--- pages/photographers/[photographersName].js
+++ pages/photographers/[photographersName].js
@@ -2,41 +2,45 @@ import prisma from "@/components/prisma"
 import Header from "@/components/header"
 import Image from "next/image"
 import Link from "next/link"
-import {ShowPhotographerImage} from "@/components/showImages"
+import { ShowPhotographerImage } from "@/components/showImages"
 
 
 export default function photographersName({ photographer, photos }) {
   const { info, personID } = photographer
 
   return (
-    <div>
+    <div className='bg-custom-grey'>
       <Header></Header>
-      <div>
-        <h1>{photographer.firstName + " " + photographer.lastName}</h1>
-        <h3>Read more about our photographer {photographer.firstName + " " + photographer.lastName}</h3>
+      <div className="bg-[url('/appcontent/18048EB5-ACE3-499A-AFE5-D0CCB02513BC.JPG')] h-96 pt-4">
+        <h1 className="text-center text-7xl text-white mb-3">{photographer.firstName + " " + photographer.lastName}</h1>
+        <p className="text-center text-lg text-white">Read more about our photographer {photographer.firstName + " " + photographer.lastName}</p>
+      </div>
+      <div className="flex justify-center  px-8">
+        <div className="flex-1 flex justify-center m-4">
+          <Image src={"/appcontent/18048EB5-ACE3-499A-AFE5-D0CCB02513BC.JPG"} alt="Image of photographer" width={300} height={300} />
+        </div>
+        <div className="flex-1 text-white">
+          <h2 className="text-3xl mb-3">{photographer.firstName + " " + photographer.lastName}</h2>
+          <h6 className="mb-3">photographer since {info.careerStart}</h6>
+
+          <h6 className="inline-block mb-3 font-bold">About:</h6>
+          <p className="inline break-normal"> {info.about}</p>
+
+          <h6 className=" mb-3 font-bold">Lens of choice:</h6>
+          <p className="inline break-normal">{info.lens}</p>
+
+          <h6 className=" mb-3 font-bold">Favorite Photo:</h6>
+          <p className="inline break-normal">{info.favoritePhoto}</p>
+
+          <h6 className=" mb-3 font-bold">Favorite thing to photograph</h6>
+          <p className="inline break-normal">{info.photoPreference}</p>
+        </div>
       </div>
       <div>
-        <h4>About the photographer</h4>
-        <h6>Read more about the photographer here</h6>
+        {/* <button><Link href={"#"}>See all of {photographer.firstName}s photographs</Link></button> */}
       </div>
       <div>
-       {/*  <Image src={"/#"} alt="Image of photographer" width={300} height={300} /> */}
-        <h2>{photographer.firstName + " " + photographer.lastName}</h2>
-        <h6>photographer since {info.careerStart}</h6>
-        <h6>About:</h6>
-        <p>{info.about}</p>
-        <h6>Lens of choice:</h6>
-        <p>{info.lens}</p>
-        <h6>Favorite Photo:</h6>
-        <p>{info.favoritePhoto}</p>
-        <h6>Favorite thing to photograph</h6>
-        <p>{info.photoPreference}</p>
-      </div>
-      <div>
-        <button><Link href={"#"}>See all of {photographer.firstName}s photographs</Link></button>
-      </div>
-      <div>
-        <h4>{photographer.firstName}&apos;s top collections</h4>
+        <h4 className="text-4xl text-white mb-3">{photographer.firstName}&apos;s top collections</h4>
       </div>
       {ShowPhotographerImage(personID)}
     </div>
@@ -48,7 +52,7 @@ export async function getStaticProps(context) {
 
 
   try {
-    
+
     const photographer = await prisma.photographer.findUnique({
       where: {
         user: photographersName
--- pages/photographers/index.js
+++ pages/photographers/index.js
@@ -1,30 +1,43 @@
 import Link from "next/link"
 import Header from "@/components/header"
 import prisma from "@/components/prisma"
+import Image from "next/image"
 
 export default function index({ photografers }) {
   return (
-    <div>
+    <>
       <Header></Header>
+      <div className='bg-custom-grey min-h-screen'>
+        <h1 className='text-4xl font-bold mb-5'>Our photographers</h1>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {photografers.map(photographer => {
+            return (
+              <div key={photographer.personID} className=' m-4 rounded overflow-hidden shadow-lg bg-gradient-to-tr from-gray-300 shadow-md'>
+                <div className='px-6 py-4'>
+                  <div className='font-bold text-xl mb-2'>
+                    <Link href={`./photographers/${photographer.user}`}>{photographer.user}</Link>
+                    <Image
+                    src="/avatar-2a011dc65ff512e320f013780071ae03.jpg"
+                    alt="kitten"
+                    width="300"
+                    height="500"
+                    />
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
 
-
-      <h1>Our photographers</h1>
-      {photografers.map(photographer => {
-        return (
-          <div key={photographer.personID}>
-            <Link href={`./photographers/${photographer.user}`}>{photographer.user}</Link>
-          </div>
-        )
-      })}
-
-    </div>
+      </div>
+    </>
   )
 }
 
 export async function getServerSideProps() {
 
   try {
-    
+
     const photografers = await prisma.photographer.findMany()
 
     return {
@@ -36,5 +49,5 @@ export async function getServerSideProps() {
     await prisma.$disconnect()
   }
 
-  
+
 }
