064a7cfd241df859cafb37075caeea938a9e8b80
Joakim <joakim@spprod.se>
2023-08-08 16:25:27 +0200

    Remove toUpperCase function

--- pages/photographers/edit/formInput.js
+++ pages/photographers/edit/formInput.js
@@ -8,7 +8,7 @@ export default function FormInput(props) {
 
   return (
     <div>
-        <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor={inputName}>{inputName.toUpperCase()}</label>
+        <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor={inputName}>{inputName}</label>
       <input type={type} name={inputName} value={value} className="shadow-sm w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
       <br />
     </div>
