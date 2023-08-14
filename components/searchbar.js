5ebcf9efc04be527868f95c825acd6e03abf7ca6
JoakimSP <joakim@spprod.se>
2023-08-14 16:03:34 +0200

    Add UI for searchbar

--- components/searchbar.js
+++ components/searchbar.js
@@ -1,28 +1,59 @@
-import React, { useState } from 'react';
+import { useState } from "react"
 
 function SearchBar() {
-  const [category, setCategory] = useState("Category 1");
+  const [view, setView] = useState("hidden")
+  const [categorie, setCategorie] = useState("All categories")
+  const toggleDropDown = () => {
+    if(view == ""){
+      setView("hidden")
+    }
+    else {
+      setView("")
+    }
+  }
+
+ console.log(categorie)
 
   return (
-    <div className="flex items-center">
-      <select 
-        className="border rounded p-2 mr-2" 
-        value={category} 
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value="Category 1">Category 1</option>
-        <option value="Category 2">Category 2</option>
-        <option value="Category 3">Category 3</option>
-        <option value="Category 4">Category 4</option>
-      </select>
-      
-      <input 
-        className="border rounded p-2" 
-        type="text" 
-        placeholder={`Search in ${category}`} 
-      />
-    </div>
-  );
+
+    <form className="w-1/2">
+      <div className="flex relative">
+        <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+        <button onClick={toggleDropDown}  className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{categorie}<svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+        </svg></button>
+        <div className={`z-10 absolute top-11 ${view} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+          <li>
+              <button onClick={() => setCategorie("All categories")} type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All categories</button>
+            </li>
+            <li>
+              <button onClick={() => setCategorie("Sunset")} type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sunset</button>
+            </li>
+            <li>
+              <button onClick={() => setCategorie("Flowers")} type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Flowers</button>
+            </li>
+            <li>
+              <button onClick={() => setCategorie("Family")} type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Family</button>
+            </li>
+            <li>
+              <button onClick={() => setCategorie("People")} type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">People</button>
+            </li>
+          </ul>
+        </div>
+        <div className="relative w-full">
+          <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required/>
+            <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+              </svg>
+              <span className="sr-only">Search</span>
+            </button>
+        </div>
+      </div>
+    </form>
+
+  )
 }
 
 export default SearchBar;
