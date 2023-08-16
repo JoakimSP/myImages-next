ccae3dd72ecfe747d53a31560880d2577dcd5275
JoakimSP <joakim@spprod.se>
2023-08-16 09:16:06 +0200

    Remove button and update email

--- components/errorBoundery.js
+++ components/errorBoundery.js
@@ -27,13 +27,8 @@ class ErrorBoundary extends Component {
           <img src="/path-to-error-image.svg" alt="Error Illustration" className="mx-auto w-48 mb-4" />
           <h1 className="text-2xl font-bold mb-2">Oops! Something went wrong.</h1>
           <p className="mb-4">We're sorry for the inconvenience.</p>
-          <button 
-              onClick={handleRetry} 
-              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-          >
-              Retry
-          </button>
-          <p className="mt-4">If the problem persists, please <a href="mailto:support@yourapp.com" className="text-blue-500 hover:underline">contact our support</a>.</p>
+          
+          <p className="mt-4">If the problem persists, please <a href="mailto:pernilla@spprod.se" className="text-blue-500 hover:underline">contact our support</a>.</p>
         </div>
       );
       
