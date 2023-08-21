18ece80527ee75be2c21e876a7d70582bfcd0221
JoakimSP <joakim@spprod.se>
2023-08-21 11:53:11 +0200

    Add privacypolicy page and textedit functionality

---
+++ pages/api/application/createPrivacyText.js
@@ -0,0 +1,26 @@
+import prisma from "@/components/prisma"
+import { logger } from "@/components/utils/logger"
+
+export default async function handler(req, res) {
+
+     try {
+       await prisma.privacypolicy.update({
+        where: {
+            id: "1"
+        },
+        data : {
+            text: req.body
+        }
+       })
+
+       res.status(200).json({ message: 'New text added' })
+    } catch (error) {
+        logger.logger.log('error', {
+            message: error.message,
+            stack: error.stack
+        })
+        res.status(500).json({Error: error})
+    } 
+    res.status(200).json({ message: 'New text added' })
+    
+}
---
+++ pages/information/privacyPolicy.js
@@ -0,0 +1,77 @@
+import { useState, useMemo } from "react";
+import dynamic from "next/dynamic";
+import Header from '@/components/header';
+import { useSession } from "next-auth/react"
+import { toast } from "react-toastify";
+import prisma from "@/components/prisma";
+import 'react-quill/dist/quill.snow.css';
+
+
+export default function PrivacyPolicy({ text }) {
+    const { data: session } = useSession()
+    const [value, setValue] = useState(text.text.toString());
+
+    const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
+    console.log(text)
+
+
+    const handleSaveInput = async () => {
+        const response = await fetch("../api/application/createPrivacyText", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(value),
+        });
+
+        if (response.ok) {
+            toast("Saved privacy text")
+        }
+        else {
+            toast.warn("Could not add categorie")
+
+        }
+    }
+
+    return (
+        <>
+            <Header />
+            {session && session.user && session.user.email === "jocke@live.se" ? (
+                <div className='w-4/5 m-auto'>
+                    {ReactQuill ? <ReactQuill theme="snow" value={value} onChange={setValue} /> : null}
+                    <button onClick={handleSaveInput} className="py-2 px-6 bg-custom-grey text-white font-semibold rounded-md hover:bg-opacity-90 transition-all duration-300">Save</button>
+                </div>
+            ) : (
+                <div className="w-4/5 m-auto">
+                    <style jsx global>{`
+                        .custom-css h1{
+                            font-weight: 500;
+                            font-size: 2rem;
+                        } 
+                    `}</style>
+                    <div className="custom-css" dangerouslySetInnerHTML={{ __html: text.text }}></div>
+                </div>
+            )}
+        </>
+    );
+    
+}
+
+
+
+export async function getServerSideProps() {
+
+    const text = await prisma.privacypolicy.findFirst({
+        where: {
+            id: "1"
+        },
+        select: {
+            text: true
+        }
+    })
+
+
+    return {
+        props: { text }
+    }
+}
