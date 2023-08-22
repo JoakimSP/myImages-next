46086a09528d19480a884ae7be93871f546c7ede
JoakimSP <joakim@spprod.se>
2023-08-22 09:13:52 +0200

    Remove edit function

--- pages/information/privacyPolicy.js
+++ pages/information/privacyPolicy.js
@@ -1,47 +1,17 @@
-import { useState, useMemo } from "react";
-import dynamic from "next/dynamic";
+import { useState } from "react";
 import Header from '@/components/header';
-import { useSession } from "next-auth/react"
-import { toast } from "react-toastify";
 import prisma from "@/components/prisma";
-import 'react-quill/dist/quill.snow.css';
 
 
 export default function PrivacyPolicy({ text }) {
-    const { data: session } = useSession()
+
     const [value, setValue] = useState(text && text.text ? text.text.toString() : "");
 
-    const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
-    console.log(text)
 
-
-    const handleSaveInput = async () => {
-        const response = await fetch("../api/application/createPrivacyText", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(value),
-        });
-
-        if (response.ok) {
-            toast("Saved privacy text")
-        }
-        else {
-            toast.warn("Could not add categorie")
-
-        }
-    }
 
     return (
         <>
             <Header />
-            {session && session.user && session.user.email === "jocke@live.se" ? (
-                <div className='w-4/5 m-auto'>
-                    {ReactQuill ? <ReactQuill theme="snow" value={value} onChange={setValue} /> : null}
-                    <button onClick={handleSaveInput} className="py-2 px-6 bg-custom-grey text-white font-semibold rounded-md hover:bg-opacity-90 transition-all duration-300">Save</button>
-                </div>
-            ) : (
                 <div className="w-4/5 m-auto">
                     <style jsx global>{`
                         .custom-css h1{
@@ -51,7 +21,6 @@ export default function PrivacyPolicy({ text }) {
                     `}</style>
                     <div className="custom-css" dangerouslySetInnerHTML={{ __html: text && text.text ? text.text : "" }}></div>
                 </div>
-            )}
         </>
     );
     
