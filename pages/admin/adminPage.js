a107bf4c9e7312a667fb08184fdbc3164fdf98d4
JoakimSP <joakim@spprod.se>
2023-08-22 09:14:36 +0200

    Add submeny to toggle view. Add edit privacy text

--- pages/admin/adminPage.js
+++ pages/admin/adminPage.js
@@ -1,25 +1,61 @@
-import { useRouter } from "next/router";
-import { toast } from "react-toastify";
+import { useState } from "react";
 import prisma from "@/components/prisma";
 import AddNewPhotographer from "@/components/addNewPhotographer";
 import AddNewCategory from "@/components/addNewCategory";
 const logger = require('@/components/utils/logger')
 import Header from "@/components/header";
+import EditPrivacyPolicy from "@/components/editPrivacyPolicy";
 
-export default function AdminPage({ photographers, categories }) {
+export default function AdminPage({ photographers, categories, policyText }) {
 
+  const [activeView, setActiveView] = useState(null);
 
+  const renderActiveView = () => {
+    switch(activeView) {
+      case 'photographers':
+        return <AddNewPhotographer photographers={photographers} />;
+      case 'categories':
+        return <AddNewCategory categories={categories} />;
+      case 'privacy':
+        return <EditPrivacyPolicy text={policyText}/>;
+      default:
+        return null;
+    }
+  }
 
   return (
     <>
     <Header/>
     <div className="bg-custom-grey min-h-screen flex flex-col justify-center items-center px-6 md:px-24">
-
-      
-      <AddNewPhotographer photographers={photographers} />
-      <AddNewCategory categories={categories} />
-
-
+      <div className="mb-auto">
+        <ul className="text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+        <li className="w-full">
+            <p 
+               onClick={() => setActiveView('photographers')} 
+               className={`inline-block w-full p-4 focus:ring-4 focus:ring-blue-300 ${activeView === 'categories' ? 'text-white bg-blue-600' : 'text-gray-900 bg-white hover:text-gray-700 hover:bg-gray-50'} focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700`}
+            >
+              photosettings
+            </p>
+          </li>
+          <li className="w-full">
+            <p 
+               onClick={() => setActiveView('categories')} 
+               className={`inline-block w-full p-4 focus:ring-4 focus:ring-blue-300 ${activeView === 'categories' ? 'text-white bg-blue-600' : 'text-gray-900 bg-white hover:text-gray-700 hover:bg-gray-50'} focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700`}
+            >
+              photosettings
+            </p>
+          </li>
+          <li className="w-full">
+            <p 
+               onClick={() => setActiveView('privacy')} 
+               className={`inline-block w-full p-4 focus:ring-4 focus:ring-blue-300 ${activeView === 'privacy' ? 'text-white bg-blue-600' : 'text-gray-900 bg-white hover:text-gray-700 hover:bg-gray-50'} focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700`}
+            >
+              Privacy
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col pb-44">{renderActiveView()}</div>
     </div>
     </>
   );
@@ -32,12 +68,21 @@ export async function getServerSideProps(context) {
 
     const photographers = await prisma.photographer.findMany()
     const categories = await prisma.categories.findMany()
+    const policyText = await prisma.privacypolicy.findFirst({
+      where: {
+          id: "1"
+      },
+      select: {
+          text: true
+      }
+  });
 
 
     return {
       props: {
         photographers,
-        categories: JSON.parse(JSON.stringify(categories))
+        categories: JSON.parse(JSON.stringify(categories)),
+        policyText
       }
     }
 
@@ -49,8 +94,9 @@ export async function getServerSideProps(context) {
     return {
       props: {
         photographers: [],
-        categories: []
-      }  // you should return an object in every condition
+        categories: [],
+        policyText: null
+      }  
     }
   } finally {
     await prisma.$disconnect()
