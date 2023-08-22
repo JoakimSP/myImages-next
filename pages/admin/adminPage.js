d3abea1c594015c58e8e39a4080aae6f73c54109
JoakimSP <joakim@spprod.se>
2023-08-22 09:17:05 +0200

    Remove double photosettings text

--- pages/admin/adminPage.js
+++ pages/admin/adminPage.js
@@ -34,7 +34,7 @@ export default function AdminPage({ photographers, categories, policyText }) {
                onClick={() => setActiveView('photographers')} 
                className={`inline-block w-full p-4 focus:ring-4 focus:ring-blue-300 ${activeView === 'categories' ? 'text-white bg-blue-600' : 'text-gray-900 bg-white hover:text-gray-700 hover:bg-gray-50'} focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700`}
             >
-              photosettings
+              photographers
             </p>
           </li>
           <li className="w-full">
