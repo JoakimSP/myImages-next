4b49c93bc5fc2e6a444158e0c53bbefcb104d462
Joakim <joakim@spprod.se>
2023-09-05 09:31:09 +0200

    Add Head compoent to import css (buggfix)

--- components/showFeaturedCollection.js
+++ components/showFeaturedCollection.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import dynamic from 'next/dynamic';
 import ErrorBoundary from "./errorBoundery";
+import Head from "next/head";
 
 const Slider = dynamic(import('react-slick'), {
-  ssr: false,
+    ssr: false,
 });
 import Link from "next/link";
 import { useState, useEffect } from "react";
@@ -11,15 +12,15 @@ import { useState, useEffect } from "react";
 function Arrow(props) {
     const { className, style, onClick } = props;
     return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", }}
-        onClick={onClick}
-      />
+        <div
+            className={className}
+            style={{ ...style, display: "block", }}
+            onClick={onClick}
+        />
     );
-  }
+}
 
-export default function ShowFeaturedCollection({featuredcol}) {
+export default function ShowFeaturedCollection({ featuredcol }) {
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
@@ -27,11 +28,11 @@ export default function ShowFeaturedCollection({featuredcol}) {
     }, []);
     const nextSlide = () => {
         setCurrentIndex(prev => (prev + 4) % featuredcol.collection.length);
-      }
-    
-      const prevSlide = () => {
+    }
+
+    const prevSlide = () => {
         setCurrentIndex(prev => (prev - 4 < 0 ? featuredcol.collection.length - 4 : prev - 4));
-      }
+    }
 
     const settings = {
         dots: true,
@@ -43,63 +44,66 @@ export default function ShowFeaturedCollection({featuredcol}) {
         prevArrow: <Arrow />,
         initialSlide: 0,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
         ]
-      };
-  return (
-    <>
-    
-    <h1 className='text-7xl text-center text-white my-4'>Featured Collections</h1>
-    <div className='flex flex-col py-8 mx-8'>
-    {isMounted &&
-    <ErrorBoundary>
-      <Slider {...settings}>
-        {featuredcol.collection.map((col) => {
-          return (
-            <div key={col.id}>
-              <Link href={`/collections/viewCollections?collectionID=${col.id}`}>
-                <div className="group relative h-96">
-                  <Image
-                    src={col.image}
-                    alt="Collection image"
-                    fill={true}
-                    className="object-cover w-full"
-                    sizes="(max-width: 768px)"
-                  />
-                </div>
-                <div><h3 className="text-center text-3xl text-white">{col.name}</h3></div>
-              </Link>
+    };
+    return (
+        <>
+            <Head>
+                <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+            </Head>
+            <h1 className='text-7xl text-center text-white my-4'>Featured Collections</h1>
+            <div className='flex flex-col py-8 mx-8'>
+                {isMounted &&
+                    <ErrorBoundary>
+                        <Slider {...settings}>
+                            {featuredcol.collection.map((col) => {
+                                return (
+                                    <div key={col.id}>
+                                        <Link href={`/collections/viewCollections?collectionID=${col.id}`}>
+                                            <div className="group relative h-96">
+                                                <Image
+                                                    src={col.image}
+                                                    alt="Collection image"
+                                                    fill={true}
+                                                    className="object-cover w-full"
+                                                    sizes="(max-width: 768px)"
+                                                />
+                                            </div>
+                                            <div><h3 className="text-center text-3xl text-white">{col.name}</h3></div>
+                                        </Link>
+                                    </div>
+                                )
+                            })}
+                        </Slider>
+                    </ErrorBoundary>
+                }
             </div>
-          )
-        })}
-      </Slider>
-      </ErrorBoundary>
-      }
-    </div>
-</>
+        </>
 
-  )
+    )
 }
