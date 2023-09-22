a918e2d47896a08aa302de3de9b03f4774f67351
Joakim <joakim@spprod.se>
2023-09-22 08:53:19 +0200

    Edit page menu buttons design

--- pages/photographers/edit/[editPhotographerPage].js
+++ pages/photographers/edit/[editPhotographerPage].js
@@ -23,31 +23,31 @@ export default function EditPhotographerPage({ userdata }) {
   return (
     <Layout>
     
-      <div className="bg-custom-grey min-h-screen">
-        <div className=" min-h-screen flex flex-col justify-center items-center px-6 md:px-24">
-          <div className="mb-auto my-3">
-            <ul className="text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex">
-              <li className="w-full">
-                <p
-                  onClick={() => setActiveView('updateinfo')}
-                  className={`inline-block w-full p-4 focus:ring-4 focus:ring-blue-300 ${activeView === 'updateinfo' ? 'text-white bg-blue-600' : 'text-gray-500 bg-white hover:text-gray-700 hover:bg-gray-50'} focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700`}
-                >
-                  info
-                </p>
-              </li>
-              <li className="w-full">
-                <p
-                  onClick={() => setActiveView('uploadphoto')}
-                  className={`inline-block w-full p-4 focus:ring-4 focus:ring-blue-300 ${activeView === 'uploadphoto' ? 'text-white bg-blue-600' : 'text-gray-500 bg-white hover:text-gray-700 hover:bg-gray-50'} focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700`}
-                >
-                  Upload
-                </p>
-              </li>
+    <div className="bg-custom-grey min-h-screen">
+    <div className="min-h-screen flex flex-col justify-center items-center px-6 md:px-24">
+        <div className="mb-auto my-3">
+            <ul className="flex space-x-2 md:space-x-4 bg-white p-1 rounded-lg shadow-lg">
+                <li className="flex-1">
+                    <button
+                        onClick={() => setActiveView('updateinfo')}
+                        className={`w-full text-center py-2 px-4 rounded-lg transition-colors duration-200 ease-in focus:outline-none focus:ring-2 focus:ring-blue-500 ${activeView === 'updateinfo' ? 'text-white bg-blue-600' : 'text-gray-700 hover:bg-gray-200'}`}
+                    >
+                        Info
+                    </button>
+                </li>
+                <li className="flex-1">
+                    <button
+                        onClick={() => setActiveView('uploadphoto')}
+                        className={`w-full text-center py-2 px-4 rounded-lg transition-colors duration-200 ease-in focus:outline-none focus:ring-2 focus:ring-blue-500 ${activeView === 'uploadphoto' ? 'text-white bg-blue-600' : 'text-gray-700 hover:bg-gray-200'}`}
+                    >
+                        Upload
+                    </button>
+                </li>
             </ul>
-          </div>
-          <div className="flex w-full justify-center pb-44">{renderActiveView()}</div>
         </div>
-      </div>
+        <div className="flex w-full justify-center pb-44">{renderActiveView()}</div>
+    </div>
+</div>
 
     </Layout>
   )
