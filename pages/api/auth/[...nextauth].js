db6452731511c8485b9abc41262de2853aa7bba7
Joakim <joakim@spprod.se>
2023-09-25 06:08:39 +0200

    Add custom sign in page

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,58 +1,64 @@
-import NextAuth from "next-auth/next"
-import GoogleProvider from "next-auth/providers/google"
-import prisma from "@/components/prisma"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from 'bcrypt'
-const logger = require('@/components/utils/logger')
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import prisma from "@/components/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt';
+const logger = require('@/components/utils/logger');
 
+// Credentials Provider Configuration
+const credentialsProvider = CredentialsProvider({
+  type: 'credentials',
+  credentials: {
+    email: { label: "email", type: "text" },
+    password: { label: "Password", type: "password" }
+  },
+  async authorize(credentials, req) {
+    const { email, password } = credentials;
 
+    try {
+      const user = await prisma.photographer.findUnique({
+        where: { email }
+      });
+      if (!user) { return null }
 
-export default NextAuth({
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) { return null }
+
+      return user;
+
+    } catch (error) {
+      logger.logger.log('error', {
+        message: error.message,
+        stack: error.stack
+      });
+      return null;
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+});
+
+// Google Provider Configuration
+const googleProvider = GoogleProvider({
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET
+});
+
+// Next Auth Options
+export const authOptions = {
   session: {
     strategy: 'jwt'
   },
-  providers: [
-    CredentialsProvider({
-      type: 'credentials',
-      credentials: {
-        email: { label: "email", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        const { email, password } = credentials
-
-        try {
-          const user = await prisma.photographer.findUnique({
-            where: {
-              email: email,
-            }
-          })
-          if (!user) { return null }
-
-          const isPasswordValid = await bcrypt.compare(password, user.password)
-          if (!isPasswordValid) { return null }
-
-          return user
-
-        } catch (error) {
-          logger.logger.log('error', {
-            message: error.message,
-            stack: error.stack
-        })
-          return null
-        } finally{
-          await prisma.$disconnect()
-        }
-
-      }
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }),
-  ],
+  providers: [credentialsProvider, googleProvider],
   secret: process.env.JWT_SECRET,
   database: process.env.DATABASE_URL,
   adapter: PrismaAdapter(prisma),
-})
+
+  pages: {
+    signIn: '/auth/signin', // Custom sign-in page path
+    // ... you can override other pages if needed
+  },
+};
+
+export default NextAuth(authOptions);
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,6 +1,7 @@
 // pages/auth/signin.js
 import { signIn } from "next-auth/react";
 import React, { useRef } from "react";
+import Image from "next/image";
 
 /* import { signIn, csrfToken } from "next-auth/react"; */
 
@@ -8,7 +9,8 @@ export default function SignIn() {
     const email = useRef("");
     const pass = useRef("");
 
-    const onSubmit = async () => {
+    const onSubmit = async (e) => {
+        e.preventDefault()
         const result = await signIn("credentials", {
             email: email.current,
             password: pass.current,
@@ -18,18 +20,97 @@ export default function SignIn() {
     };
 
     return (
-        <div>
-            <div className="mb-6">
-                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" onChange={(e) => (email.current = e.target.value)} required />
-                
+        <section className="h-screen">
+            <div className="flex w-full h-full">
+                <div className="flex flex-1 h-full flex-wrap justify-center lg:justify-between">
+                    {/* <!-- Left column container with background--> */}
+                    <div className="absolute md:relative flex-2 mb-12 md:mb-0  h-full w-full md:w-8/12 ">
+                        <Image
+                            src="/appcontent/1ADE38D0-8B0E-46FB-8EB7-7F50927BCA21.JPG"
+                            className=" object-cover"
+                            fill={true}
+                            alt="Phone image"
+                        />
+                    </div>
+
+                    {/* <!-- Right column container with form --> */}
+                    <div className="flex-1 flex flex-col justify-center mx-6 w-5/12 z-10">
+                        <form>
+                            {/* <!-- Email input --> */}
+                            <div className="mb-6">
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    Email Address
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    placeholder="Email address"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    onChange={(e) => (email.current = e.target.value)}
+                                    required
+                                />
+                            </div>
+
+                            {/* <!--Password input--> */}
+                            <div className="mb-6">
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    placeholder="Password"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    onChange={(e) => (pass.current = e.target.value)}
+                                    required
+                                />
+                            </div>
+
+                            {/* <!-- Remember me checkbox --> */}
+                            <div className="mb-6 flex items-center justify-between">
+
+
+                                {/* <!-- Forgot password link --> */}
+                                <a
+                                    href="#!"
+                                    className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+                                >
+                                    Forgot password?
+                                </a>
+                            </div>
+
+                            {/* <!-- Submit button --> */}
+
+                            <button
+                                onClick={onSubmit}
+                                className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal bg-slate-50 text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                                Sign in
+                            </button>
+
+                            {/* <!-- Divider --> */}
+                            <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                                <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
+                                    OR
+                                </p>
+                            </div>
+
+                            {/* <!-- Social login buttons --> */}
+
+                        </form>
+                        <button
+                            className="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                            style={{ backgroundColor: "#3b5998" }}
+                            onClick={() => signIn("google")}
+                            role="button"
+                        >
+                            <svg class="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+                                <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd" />
+                            </svg>
+                            Continue with Google
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="mb-6">
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => (pass.current = e.target.value)} required />
-            </div>
-            
-            <button onClick={onSubmit} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-        </div>
+        </section>
     );
 }
