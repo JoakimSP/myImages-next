8a294fc757a6eabbd6ed9fee0ba75f314a8746ae
Joakim <joakim@spprod.se>
2023-09-28 08:55:54 +0200

    Update upload image function to store 4 images local

--- components/editphotographerinfo/uploadImage.js
+++ components/editphotographerinfo/uploadImage.js
@@ -1,87 +1,52 @@
-import { storage } from "@/components/firebase";
-import { ref, uploadBytes, getDownloadURL, getMetadata } from "firebase/storage";
 import { v4 } from "uuid";
 import { useRef, useState } from "react"
 import UploadProfilePicture from "./uploadProfilePicture";
 import UploadPhotographersHero from "./uploadPhotographersHero";
+import { toast } from "react-toastify";
 
 export default function UploadImage({ userdata }) {
-    const [imageUpload, setImageUpload] = useState()
+    const [imagesUpload, setImagesUpload] = useState([])
     const aspectImage = useRef()
 
-
     const uploadImage = async () => {
-        if (imageUpload == null) return;
-        const imageName = imageUpload.name + v4();
+        if (!imagesUpload.length) return;
 
-        const imageRefUser = ref(storage, `${userdata.personID}/${imageName}`);
-        const imageRef = ref(storage, `${imageName}`);
+        const formData = new FormData();
+        imagesUpload.forEach((image, index) => {
+            const imageName = v4() + image.name;
+            formData.append(`image[]`, image, imageName);
 
+            const photoInformation = {
+                personID: userdata.personID,
+                filename: imageName,
+                filetype: image.type,
+                filesize: image.size,
+            };
+
+            formData.append(`photoInformation[]`, JSON.stringify(photoInformation));
+        });
 
         try {
-            // Upload to user-specific directory
-            await uploadBytes(imageRefUser, imageUpload);
+            const res = await fetch('/api/testUpload', {
+                method: 'POST',
+                body: formData
+            });
 
-            // Upload to general directory
-            await uploadBytes(imageRef, imageUpload);
-
-            // Get the download URL
-            const url = await getDownloadURL(imageRef);
-            const urlUser = await getDownloadURL(imageRefUser);
-
-            const { width, height } = await renderImageTogetAspectRatio(url);
-
-            
-
-            // Pass URL to uploadData function
-            uploadImageData(url, urlUser, width, height);
-            window.alert("image uploaded");
+            if (res.ok) {
+                toast("Image is uploaded");
+            } else {
+                toast("Error uploading image");
+                console.error("Error status:", res.status);
+            }
         } catch (error) {
-
-            console.error("Error uploading image: ", error);
+            toast("Error uploading image");
+            console.error(error);
         }
     };
-    const uploadImageData = async (imageUrl, imageUrlUser, width, height) => {
 
-        
-        const fileName = imageUpload.name + v4();
-        const photoInformation = {
-            personID: userdata.personID,
-            filename: fileName,
-            filetype: imageUpload.type,
-            filesize: imageUpload.size,
-            url: imageUrl,
-            urlUser: imageUrlUser,
-            width: width,
-            height: height
 
-        }
 
-        const res = await fetch('../../api/images/storeImages', {
-            method: "POST",
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(photoInformation)
-        })
-    }
 
-    const renderImageTogetAspectRatio = (url) => {
-        return new Promise((resolve) => {
-            aspectImage.current.onload = () => {
-                const width = aspectImage.current.naturalWidth;
-                const height = aspectImage.current.naturalHeight;
-    
-                
-                resolve({ width, height });
-            };
-    
-            aspectImage.current.src = url;
-        });
-    };
-    
-             
-            
 
     return (
 
@@ -89,32 +54,31 @@ export default function UploadImage({ userdata }) {
         <div className="grid grid-cols-2">
             <UploadPhotographersHero userdata={userdata} />
             <UploadProfilePicture userdata={userdata} />
-            
+
             <div className="max-w-5xl mx-auto mt-12">
-            <h1 className="text-center text-4xl font-semibold text-white mt-12 mb-6 dark:text-white">Upload a photo</h1>
+                <h1 className="text-center text-4xl font-semibold text-white mt-12 mb-6 dark:text-white">Upload a photo</h1>
                 <div className="flex flex-col items-center space-y-6">
                     {/* Drop zone */}
                     <div
                         onDrop={(e) => {
                             e.preventDefault();
                             const files = e.dataTransfer.files;
-                            if (files.length) {
-                                setImageUpload(files[0]);
-                            }
+                            setImagesUpload(prevImages => [...prevImages, ...files]);
                         }}
                         onDragOver={(e) => e.preventDefault()}
                         className="flex justify-center items-center w-60 h-60 border-dotted border-4 border-gray-400 hover:border-gray-600 cursor-pointer bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-800"
                     >
-                        <p className="text-gray-800 text-center dark:text-gray-300">{imageUpload ? imageUpload.name : "Drop image here"}</p>
+                        <p className="text-gray-800 text-center dark:text-gray-300">{imagesUpload ? imagesUpload.name : "Drop image here"}</p>
                     </div>
 
                     {/* File Input */}
                     <input
-                        onChange={(e) => { setImageUpload(e.target.files[0]) }}
+                        onChange={(e) => { setImagesUpload([...e.target.files]) }}
                         className="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-white dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                         aria-describedby="user_avatar_help"
                         id="user_avatar"
                         type="file"
+                        multiple
                     />
 
                     {/* Upload Button */}
@@ -126,7 +90,7 @@ export default function UploadImage({ userdata }) {
                     </button>
                 </div>
             </div>
-            <img src="" alt="" ref={aspectImage} className="max-w-0 max-h-0"/>
+            <img src="" alt="" ref={aspectImage} className="max-w-0 max-h-0" />
         </div>
     )
 }
--- pages/api/testUpload.js
+++ pages/api/testUpload.js
@@ -1,15 +1,146 @@
-import getRawBody from 'raw-body';
-import { parse } from 'querystring';
-import { createWriteStream, mkdirSync } from 'fs';
+import { mkdirSync, unlinkSync } from 'fs';
 import { join } from 'path';
+import prisma from '@/components/prisma';
+import multer from 'multer';
+import nextConnect from 'next-connect';
+import sharp from 'sharp';
+import { getSession } from 'next-auth/react';
 
+
+//turn of nextjs request object parser
 export const config = {
   api: {
     bodyParser: false,
   },
 };
 
-export default async (req, res) => {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = join(process.cwd(), 'images', file.originalname);
+    mkdirSync(dir, { recursive: true });
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+
+const upload = multer({
+  storage,
+  limits: {
+    files: 10
+  }
+})
+
+const handler = nextConnect()
+
+handler.use(async (req, res, next) => {
+  const session = await getSession({ req });
+  if (session) {
+    req.userEmail = session.user.email;
+    next();
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+})
+
+
+handler.post(async (req, res) => {
+  console.log("POST handler initiated");
+  await new Promise((resolve, reject) => {
+    upload.array("image[]")(req, res, async (err) => {
+      if (err) {
+        console.log(err)
+        reject(err);
+        return res.status(500).json({ error: "Image upload failed", details: err.message });
+      }
+      resolve()
+
+      const files = req.files;
+      const photoInformationArray = req.body.photoInformation;
+      console.log(photoInformationArray)
+
+      for (let i = 0; i < files.length; i++) {
+        const parsedPhotoInformation = JSON.parse(photoInformationArray[i]);
+        const file = files[i]
+        const {
+          personID,
+          filename,
+          filetype,
+          filesize
+        } = parsedPhotoInformation
+        const imageMetadata = await sharp(file.path).metadata();
+
+        if (imageMetadata.width >= imageMetadata.height) {
+          await processAndStoreImage(file, 'small', 1000, null, personID, filename, filetype, filesize, imageMetadata);
+          await processAndStoreImage(file, 'medium', 2400, null, personID, filename, filetype, filesize, imageMetadata);
+          await processAndStoreImage(file, 'large', 4000, null, personID, filename, filetype, filesize, imageMetadata);
+        } else {
+          await processAndStoreImage(file, 'small', null, 1000, personID, filename, filetype, filesize, imageMetadata);
+          await processAndStoreImage(file, 'medium', null, 2760, personID, filename, filetype, filesize, imageMetadata);
+          await processAndStoreImage(file, 'large', null, 5520, personID, filename, filetype, filesize, imageMetadata);
+        }
+
+        // Store the original
+        const originalPath = join(file.destination, `${file.filename}.tiff`);
+        await sharp(file.path).toFile(originalPath);
+        await prisma.photos.create({
+          data: {
+            personID: personID,
+            filename: filename,
+            filetype: filetype,
+            filesize: filesize,
+            filepath: originalPath,
+            size: 'original',
+            width: imageMetadata.width,
+            height: imageMetadata.height
+          }
+
+        });
+
+        // Optional: Remove the original uploaded image to free up space
+        unlinkSync(file.path);
+      }
+      prisma.$disconnect()
+      res.status(200).json({ message: "Images uploaded", files: req.files });
+    })
+  })
+
+})
+
+async function processAndStoreImage(image, size, resizeWidth, resizeHeight, personID, filename, filetype, filesize, imageMetadata) {
+  const outputPath = resizeWidth ?
+    join(image.destination, `${resizeWidth}-${image.filename}`) :
+    join(image.destination, `${resizeHeight}-${image.filename}`);
+
+  await sharp(image.path).resize(resizeWidth, resizeHeight).toFile(outputPath);
+
+  await prisma.photos.create({
+    data: {
+      personID: personID,
+      filename: filename,
+      filetype: filetype,
+      filesize: filesize,
+      filepath: outputPath,  // Single filepath column in the database
+      size: size,  // Store the size value ("small", "medium", etc.)
+      width: imageMetadata.width,
+      height: imageMetadata.height
+    }
+  });
+}
+
+export default handler;
+
+
+
+
+
+
+
+
+
+/* export default async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).end();
   }
@@ -18,22 +149,60 @@ export default async (req, res) => {
   try {
     raw = await getRawBody(req, {
       length: req.headers['content-length'],
-      limit: '100mb',  // Adjust this limit based on your requirements
+      limit: '500mb',  // Adjust this limit based on your requirements
     });
   } catch (e) {
     res.status(413).json({ error: 'Body exceeded limit' });
     return;
   }
-
   const data = parse(raw.toString());
+  data.photoInformation = JSON.parse(data.photoInformation)
   const imageBuffer = Buffer.from(data.image.replace(/^data:image\/\w+;base64,/, ""), 'base64');
+  const {
+    personID,
+    filename,
+    filetype,
+    filesize,
+    width,
+    height
+  } = data.photoInformation
 
   // Ensure the directory exists
-  const dir = join(process.cwd(), 'images');
+  const dir = join(process.cwd(), 'images', filename);
   mkdirSync(dir, { recursive: true });
-  const imagePath = join(dir, `imagename.png`); // Assuming PNG, adjust if necessary
+  const imagePathSmall = join(dir, `small-${filename}`); 
+  const imagePathMedium = join(dir, `medium-${filename}`); 
+  const imagePathLarge = join(dir, `large-${filename}`); 
+  const imagePathOriginal = join(dir, `${filename}`); 
 
-  createWriteStream(imagePath).end(imageBuffer);
+  createWriteStream(imagePathSmall).end(imageBuffer);
+  createWriteStream(imagePathMedium).end(imageBuffer);
+  createWriteStream(imagePathLarge).end(imageBuffer);
+  createWriteStream(imagePathOriginal).end(imageBuffer);
+
+
+  try {
+    await prisma.photos.create({
+      data: {
+        personID: personID,
+        filename: filename,
+        filetype: filetype,
+        filesize: filesize,
+        filepathsmall : imagePathSmall,
+        filepathmedium : imagePathMedium,
+        filepathlarge : imagePathLarge,
+        filepathoriginal : imagePathLarge,
+        width: width,
+        height: height
+      }
+    })
+    prisma.$disconnect()
+  } catch (error) {
+    console.log(error) 
+  }
 
   res.status(200).json({ message: 'Image uploaded!', path: `/images/imagename.png` });
-};
+}; */
+
+
+
