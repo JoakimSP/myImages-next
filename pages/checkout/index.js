c7f666d8a3d6a6126b8c2b8177ea3df6a40a3e92
Joakim <joakim@spprod.se>
2023-10-24 16:18:39 +0200

    Add some style to checkout

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -34,14 +34,33 @@ export default function Index({lastReceipt}) {
   return (
     <Layout>
       <ErrorBoundary>
-      <div>
-        <h1>Thank you for your payment!</h1>
-        <p>A receipt has been sent via email</p>
-        <h3>Total payment amount</h3>
-        <h1>{formatCurrency(sumOfCart)}</h1>
-      
-        <button onClick={handleDownloadImage}>Here is your link to download the image</button>
-      </div>
+        <div className="flex items-center justify-center min-h-full mt-6">
+          <div className="bg-white bg-opacity-95 p-8 w-4/5 mx-auto max-w-2xl rounded-lg shadow-lg">
+            <h1 className="text-xl font-bold mb-5">Thank you for your payment!</h1>
+            <p className="mb-5">A receipt has been sent via email.</p>
+            <div className="grid grid-cols-2 gap-5 mb-5">
+              <div>
+                <h3 className="mb-3">Seller</h3>
+                <p>Name: ...</p>
+                <p>Email: ...</p>
+              </div>
+              <div>
+                <h3 className="mb-3">Buyer</h3>
+                <p>Name: ...</p>
+                <p>Email: ...</p>
+              </div>
+            </div>
+            <h3 className="mb-3">Image Details</h3>
+            <p>Title: ...</p>
+            <p>License Type: ...</p>
+            <p className="mb-5">Price: {formatCurrency(sumOfCart)}</p>
+            <div className="flex justify-between items-center mb-5">
+              <h1 className="text-3xl font-bold">Total Amount</h1>
+              <h1 className="text-3xl font-bold">{formatCurrency(sumOfCart)}</h1>
+            </div>
+            <button onClick={handleDownloadImage} className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full">Download Your Image</button>
+          </div>
+        </div>
       </ErrorBoundary>
     </Layout>
   )
