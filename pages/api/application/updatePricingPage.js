671f89cb9dfba35537ba6f2981e974559f7b53e5
Joakim <joakim@spprod.se>
2023-10-24 08:28:04 +0200

    Update api to not require images and to owerwrite old images if new ones are uploaded

--- pages/api/application/updatePricingPage.js
+++ pages/api/application/updatePricingPage.js
@@ -2,13 +2,21 @@ import prisma from "@/components/prisma";
 import multer from 'multer';
 import path from 'path';
 
-// Set up multer storage
+
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'images/pricingPage/');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));  // Use Date.now() to ensure unique names
+
+    if(file.fieldname === 'imageleft') {
+      cb(null, 'imageleft' + path.extname(file.originalname));
+    } else if(file.fieldname === 'imageright') {
+      cb(null, 'imageright' + path.extname(file.originalname));
+    } else {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
   }
 });
 
@@ -16,52 +24,43 @@ const upload = multer({ storage: storage });
 
 export const config = {
   api: {
-    bodyParser: false,  // Disabling Next.js's body parser as multer will handle it
+    bodyParser: false,  
   },
 };
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Handling image uploads using multer
     upload.fields([{ name: 'imageleft', maxCount: 1 }, { name: 'imageright', maxCount: 1 }])(req, res, async (err) => {
       if (err) {
         return res.status(500).json({ error: "Error uploading the images." });
       }
 
-      // Extracting image paths
+
       const imageUrlLeft = req.files.imageleft ? req.files.imageleft[0].path : null;
       const imageUrlRight = req.files.imageright ? req.files.imageright[0].path : null;
 
-      try {
-        const {
-          title,
-          subtitle,
-          imagelefttitle,
-          imageleftsubtitle,
-          imageleftprice,
-          imagerighttitle,
-          imagerightsubtitle,
-          imagerightprice,
-          footertext,
-        } = req.body;
+      const updateData = {
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        imageTitleLeft: req.body.imagelefttitle,
+        imageSubTitleLeft: req.body.imageleftsubtitle,
+        imagePriceLeft: req.body.imageleftprice,
+        imageTitleRight: req.body.imagerighttitle,
+        imageSubTitleRight: req.body.imagerightsubtitle,
+        imagePriceRight: req.body.imagerightprice,
+        footerText: req.body.footertext,
+      };
 
+
+      if(imageUrlLeft) updateData.imageUrlLeft = imageUrlLeft;
+      if(imageUrlRight) updateData.imageUrlRight = imageUrlRight;
+
+      try {
         const updatedPricingPage = await prisma.pricingpage.update({
           where: {
             id: "1"
           },
-          data: {
-            title,
-            subtitle,
-            imageUrlLeft,
-            imageTitleLeft: imagelefttitle,
-            imageSubTitleLeft: imageleftsubtitle,
-            imagePriceLeft: imageleftprice,
-            imageUrlRight,
-            imageTitleRight: imagerighttitle,
-            imageSubTitleRight: imagerightsubtitle,
-            imagePriceRight: imagerightprice,
-            footerText: footertext,
-          }
+          data: updateData
         });
 
         return res.status(200).json(updatedPricingPage);
