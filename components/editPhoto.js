479e6d669c0c8aae41e9ee7b15cf9aa98e490a63
Joakim <joakim@spprod.se>
2023-10-24 16:23:06 +0200

    Update style on form

--- components/editPhoto.js
+++ components/editPhoto.js
@@ -8,7 +8,6 @@ import { useState } from "react"
 export default function EditPhoto({ photo, categories, collections, photoCopies }) {
     const [tags, setTags] = useState(photo.tags || [])
     const photoCopiesId = photoCopies.map(copy => ({ id: copy.id, size: copy.size}));
-    console.log(collections)
 
     const handleUpdateTags = (newTags) => {
         setTags(newTags);
@@ -58,58 +57,60 @@ export default function EditPhoto({ photo, categories, collections, photoCopies 
     }
     return (
         <ErrorBoundary>
-            <form className="space-y-4" onSubmit={HandleUpdateInfo}>
+        <form className="space-y-6" onSubmit={HandleUpdateInfo}>
+            <div className="space-y-4">
                 <div>
-                    <label className="block mb-2" htmlFor="title">title</label>
+                    <label className="block text-lg mb-2" htmlFor="title">Title</label>
                     <input id="title" type="text" name="title" className="w-full p-2 border rounded" defaultValue={photo.title} placeholder={photo.title} required />
                 </div>
                 <div>
-                    <label className="block mb-2" htmlFor="description">Description</label>
+                    <label className="block text-lg mb-2" htmlFor="description">Description</label>
                     <input id="description" type="text" name="description" className="w-full p-2 border rounded" defaultValue={photo.description} placeholder={photo.description} required />
                 </div>
-                {photoCopies.map((copy, index) => {
-                    return (<div key={index}>
-                    <label className="block mb-2" htmlFor="price">Price {copy.size}</label>
+            </div>
+    
+            {photoCopies.map((copy, index) => (
+                <div key={index} className="mb-4">
+                    <label className="block text-lg mb-2" htmlFor="price">Price {copy.size}</label>
                     <input id="price" type="number" min="0" name={`price${copy.size}`} className="w-full p-2 border rounded" defaultValue={copy.price} placeholder={copy.price} required />
                 </div>
-                )})}
-                <div className="space-y-2">
-
-                    <div>
-                        <div>
-                            <legend className="font-semibold">Category</legend>
-                            <select name="categories" defaultValue={photo.categoriesId}>
-                                {categories.map((cat) => {
-                                    return (
-                                        <option key={cat.id} value={cat.id} className="mr-2">{cat.name}</option>
-                                    )
-                                })}
-                            </select>
-                        </div>
-                        <div>
-                            <legend className="font-semibold">Collections</legend>
-                            <select name="collections" defaultValue={photo.collectionId}>
-                                {collections.map((col) => {
-                                    return (
-                                        <option key={col.id} value={col.id} className="mr-2">{col.name}</option>
-                                    )
-                                })}
-                            </select>
-                        </div>
-
-                    </div>
-                </div>
+            ))}
+    
+            <div className="grid grid-cols-2 gap-4">
                 <div>
-                    <label>Should the photo exclusive?</label>
-                    <div><input type="checkbox" name="exclusive" className="w-6 h-6" /></div>
+                    <label className="block text-lg mb-2">Category</label>
+                    <select name="categories" defaultValue={photo.categoriesId} className="w-full p-2 border rounded">
+                        {categories.map((cat) => (
+                            <option key={cat.id} value={cat.id}>{cat.name}</option>
+                        ))}
+                    </select>
                 </div>
+    
                 <div>
-                    <TagsInput value={tags} onChange={handleUpdateTags} onlyUnique />
+                    <label className="block text-lg mb-2">Collections</label>
+                    <select name="collections" defaultValue={photo.collectionId} className="w-full p-2 border rounded">
+                        {collections.map((col) => (
+                            <option key={col.id} value={col.id}>{col.name}</option>
+                        ))}
+                    </select>
                 </div>
-                <div>
-                    <input type="submit" value="Submit" className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-blue-500 hover:bg-blue-700" />
-                </div>
-            </form>
-        </ErrorBoundary>
+            </div>
+    
+            <div className="flex items-center space-x-2 mt-4">
+                <input type="checkbox" name="exclusive" className="w-6 h-6" />
+                <label className="text-lg">Should the photo be exclusive?</label>
+            </div>
+    
+            <div className="mt-4">
+                <p className="text-blue-600 font-medium mb-1"><i className="info icon"></i> Tip: Type a tag and press Enter to queue it.</p>
+                <TagsInput value={tags} onChange={handleUpdateTags} onlyUnique className="w-full p-2 border rounded" />
+            </div>
+    
+            <div className="mt-6">
+                <input type="submit" value="Submit" className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-blue-500 hover:bg-blue-700 transition duration-300 ease-in-out" />
+            </div>
+        </form>
+    </ErrorBoundary>
+    
     )
 }
