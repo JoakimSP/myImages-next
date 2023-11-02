41ddbe595522314efeea34f0abd9d94330d3e79b
Joakim <joakim@spprod.se>
2023-11-02 17:18:47 +0100

    Update searchbar to be more responsive

--- components/searchbar.js
+++ components/searchbar.js
@@ -13,15 +13,15 @@ function SearchBar({ categories }) {
 
   return (
     <ErrorBoundary>
-      <form onSubmit={HandleSearch} className="w-1/2">
-        <div className="flex relative">
+      <form onSubmit={HandleSearch} className="w-full md:w-3/4 lg:w-1/2 mx-12">
+        <div className="flex flex-wrap relative">
           <label htmlFor="categoryDropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Categories</label>
-          <div className="relative flex-shrink-0 z-10">
+          <div className="relative flex-shrink-0 z-10 w-full sm:w-auto mb-2 sm:mb-0">
             <select
               id="categoryDropdown"
               value={categorie}
               onChange={(e) => setCategorie(e.target.value)}
-              className="block appearance-none py-2.5 px-4 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 w-full"
+              className="block appearance-none py-2.5 px-4 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 w-full md:rounded-r-none rounded-r-lg"
             >
               <option value="All categories">All categories</option>
               {categories.map((cat) => (
@@ -33,7 +33,7 @@ function SearchBar({ categories }) {
             </div>
           </div>
 
-          <div className="relative w-full">
+          <div className="relative w-full sm:w-auto flex-grow mb-2 sm:mb-0">
             <input type="search" name="searchDropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search photos..." />
             <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
