3b3a2482d85cb306834acfb55eaf31d1791a32b1
Joakim <joakim@spprod.se>
2023-11-06 15:24:49 +0100

    Add onChange prop to formInput

--- pages/photographers/edit/formInput.js
+++ pages/photographers/edit/formInput.js
@@ -3,13 +3,14 @@ export default function FormInput(props) {
     const {
         inputName,
         type,
-        value
+        value,
+        onChange
     } = props
 
   return (
     <div>
         <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor={inputName}>{inputName}</label>
-      <input type={type} name={inputName} value={value} className="shadow-sm w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+      <input type={type} name={inputName} value={value} onChange={onChange} className="shadow-sm w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
       <br />
     </div>
   )
