3044ec17adbc1fde767197b917f8ec9f7eb0e948
Joakim <joakim@spprod.se>
2023-11-09 12:13:21 +0100

    Update to dynamic html element

--- components/utils/inputField.js
+++ components/utils/inputField.js
@@ -1,11 +1,15 @@
-
-
-export default function InputField({ label, type, name, textColor, ...rest }) {
+export default function InputField({ label, type, name, textColor, as, ...rest }) {
+    const InputOrTextarea = as === "textarea" ? "textarea" : "input";
+    
     return (
-        <div className="my-4">
-            <label htmlFor={name} className={`block mb-2 text-${textColor} font-medium`}>{label} *</label>
-            <input type={type} name={name} id={name} {...rest} className="border rounded-md p-2 w-full focus:ring focus:ring-custom-grey-light focus:border-transparent" required/>
-        </div>
-
-    )
-}
+      <div className="my-4">
+        <label htmlFor={name} className={`block mb-2 text-${textColor} font-medium`}>{label} *</label>
+        {InputOrTextarea === "textarea" ? (
+          <textarea name={name} id={name} {...rest} className="border rounded-md p-2 w-full focus:ring focus:ring-custom-grey-light focus:border-transparent" required></textarea>
+        ) : (
+          <input type={type} name={name} id={name} {...rest} className="border rounded-md p-2 w-full focus:ring focus:ring-custom-grey-light focus:border-transparent" required/>
+        )}
+      </div>
+    );
+  }
+  
