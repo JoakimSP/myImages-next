15b3659bb9fe62eab2e88f621c7ab45677c5c551
Joakim <joakim@spprod.se>
2023-11-14 07:09:10 +0100

    Update collections images to thumbnails

--- pages/collections/[viewCollections].js
+++ pages/collections/[viewCollections].js
@@ -8,93 +8,74 @@ import Link from 'next/link';
 
 export default function ViewCollections({ collection }) {
     const [images, setImages] = useState([]);
+    const { photos } = collection;
 
+    // Group photos by title and filename
+    const groupedPhotos = photos.reduce((acc, photo) => {
+        const key = `${photo.title}-${photo.filename}`;
+        acc[key] = acc[key] || [];
+        acc[key].push(photo);
+        return acc;
+    }, {});
 
-    /* useEffect(() => {
-        if (collection.photos == null) {
-            return
-        }
-        const filepaths = collection.photos.map(photo => photo.filepath);
-
-        async function fetchImages() {
-            const response = await fetch('/api/images/getImages', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ paths: filepaths }),
-
-            });
-
-            if (response.ok) {
-                const imagesBase64 = await response.json();
-                const urls = filepaths.map(filepath => {
-                    const base64 = imagesBase64[filepath];
-                    if (base64) {
-                        return `data:image/jpeg;base64,${base64}`; // Assuming all images are jpg.
-                    }
-                    return null;
-                }).filter(Boolean);
-                setImages(urls);
-            } else {
-                console.error('Failed to fetch images.');
-            }
-        }
-
-        fetchImages();
-    }, [collection]); */
-    /* if (images.length === 0) return <p>Loading...</p> */
-
-    const twoItemRow = (index) => (index - 1) % 5 === 2 || (index - 1) % 5 === 3;
-    let counter = 0
-    const firstPhoto = collection.photos.slice(0, 1)
+    // Get the first photo's thumbnail
+    const firstPhotoKey = Object.keys(groupedPhotos)[0];
+    const firstPhotoThumb = groupedPhotos[firstPhotoKey]?.find(p => p.size === 'thumb');
+    const firstPhotoSmallWm = groupedPhotos[firstPhotoKey]?.find(p => p.size === 'small-wm');
 
 
     return (
         <Layout>
-            <div className='flex flex-col w-full h-screen' >
-                <div className="relative h-2/3">
-                    <Image src={`/api/images/viewImage?name=${collection.imagepath}`} alt={collection.name} fill={true} className={"object-cover"} />
-                    <div className='flex justify-center items-center absolute text-3xl md:text-9xl z-10 text-white w-full h-full'>
-                        <h1 className='mb-44 text-center'>{collection.name}</h1>
-                    </div>
-                </div>
-
-
-
-                <div className='flex'>
-                    <div className="flex-1 h-96 bg-custom-grey text-white text-left p-4 overflow-hidden">
-                        <div className='p-2 md:p-12 overflow-hidden'>
-                            <h2 className='text-3xl md:text-6xl  '>{collection.name}</h2>
-                            <p className='text-xl mb-8'>{collection.subtitle}</p>
-                            <p className='text-lg'>{collection.description}</p>
-                        </div>
-                    </div>
-                    {firstPhoto && firstPhoto.length > 0 && (
-                        <div className="flex-1 h-96 bg-custom-grey text-white pt-4 pr-4 md:block hidden">
-                            <div className="relative w-full h-full">
-                            <Link  href={`/images/viewimage?img=${encodeURIComponent(firstPhoto[0].filepath)}&folderpath=${firstPhoto[0].folderpath}`}>
-                                <Image src={`/api/images/viewImage?name=${firstPhoto[0].filepath}`} alt={collection.name} fill={true} className={"object-cover"}/>
-                                </Link> 
+            <div className='w-full h-auto min-h-screen' >
+                {/* Collection Header Image and Name */}
+                    <div className="min-h-[600px] man-h-[800px]">
+                        <div className='relative w-full h-[66vh]'>
+                            <Image src={`/api/images/viewImage?name=${collection.imagepath}`} alt={collection.name} fill={true} className={"object-cover"} />
+                            <div className='flex justify-center items-center absolute text-3xl md:text-9xl z-10 text-white w-full h-full'>
+                                <h1 className='mb-44 text-center'>{collection.name}</h1>
                             </div>
                         </div>
-                    )}
+                    </div>
+                    {/* Collection Description and First Photo */}
+                    <div className='flex flex-col md:flex-row'>
+                        <div className="flex-1 h-96 bg-custom-grey text-white text-left p-4 overflow-hidden">
+                            <div className='p-2 md:p-12 overflow-hidden'>
+                                <h2 className='text-3xl md:text-6xl'>{collection.name}</h2>
+                                <p className='text-xl mb-8'>{collection.subtitle}</p>
+                                <p className='text-lg'>{collection.description}</p>
+                            </div>
+                        </div>
+                        {firstPhotoThumb && (
+                            <div className="flex-1 h-96 bg-custom-grey text-white pt-4 pr-4 md:block hidden">
+                                <div className="relative w-full h-full">
+                                    <Link href={`/images/viewimage?img=${encodeURIComponent(firstPhotoSmallWm?.filepath)}&folderpath=${firstPhotoSmallWm?.folderpath}`}>
+                                        <Image src={`/api/images/viewImage?name=${firstPhotoThumb.filepath}`} alt={collection.name} fill={true} className={"object-cover"} quality={100}/>
+                                    </Link>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    {/* Collection Photos Grid */}
+                    <div className='w-full p-5 pb-10 mx-auto mb-10 gap-5 columns-1 md:columns-2 lg:columns-3 space-y-5 bg-custom-grey'>
+                        {Object.entries(groupedPhotos).map(([key, photoGroup], index) => {
+                            if (index === 0) return null; // Skip the first photo, already displayed
+                            const thumbPhoto = photoGroup.find(p => p.size === 'thumb');
+                            const smallWmPhoto = photoGroup.find(p => p.size === 'small-wm');
+                            return (
+                                <Link key={key} href={`/images/viewimage?img=${encodeURIComponent(smallWmPhoto?.filepath)}&folderpath=${smallWmPhoto?.folderpath}`}>
+                                        <Image
+                                            src={`/api/images/viewImage?name=${thumbPhoto?.filepath}`}
+                                            alt={thumbPhoto?.title || 'image'}
+                                            width={thumbPhoto?.width}
+                                            height={thumbPhoto?.height}
+                                            className="my-5"
+                                            layout='responsive' />
+                                </Link>
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
-            <div className='w-full p-5 pb-10 mx-auto mb-10 gap-5 columns-1 md:columns-2 lg:columns-3 space-y-5 bg-custom-grey'>
-                {collection.photos && collection.photos.length > 0 && (
-                    collection.photos.slice(1).map((photo, index) => {
-                        return (
-                            <Link key={index} href={`/images/viewimage?img=${encodeURIComponent(photo.filepath)}&folderpath=${photo.folderpath}`}>
-                                <Image key={photo.id} src={`/api/images/viewImage?name=${photo.filepath}`} alt="image" width={photo.width} height={photo.height} className="my-5" />
-                            </Link>
-                        );
-                    })
-                )}
-
-            </div>
-
-        </Layout >
+        </Layout>
     )
 }
 
@@ -126,7 +107,7 @@ export async function getServerSideProps(context) {
         }
 
     } catch (error) {
-        logger.logger.log('error', {
+        logger.log('error', {
             message: error.message,
             stack: error.stack
 
