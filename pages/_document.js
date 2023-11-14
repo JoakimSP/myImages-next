61bade7bafe26d59966a8731aacd07ed9b04db2c
Joakim <joakim@spprod.se>
2023-11-14 07:20:46 +0100

    Move favicon to _document

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/appcontent/camera.png"/>
       </Head>
       <body>
         <Main />
