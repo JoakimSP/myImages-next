5acc0ac3df028f561bdadc6a8d3cc7786e095671
Joakim <joakim@spprod.se>
2023-11-14 07:14:22 +0100

    update logger route

--- pages/api/images/deleteImage.js
+++ pages/api/images/deleteImage.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
             res.status(200).json({ message: "Delete image" });
         }
     } catch (error) {
-        logger.logger.log('error', {
+        logger.log('error', {
             message: error.message,
             stack: error.stack
         });
