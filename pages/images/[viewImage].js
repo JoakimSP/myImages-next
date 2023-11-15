a4c2becb86daf43ebed9f11e9e7029d5f3f448cb
Joakim <joakim@spprod.se>
2023-11-15 16:15:54 +0100

    Add size description to photos

--- pages/images/[viewImage].js
+++ pages/images/[viewImage].js
@@ -8,6 +8,8 @@ import prisma from "@/components/prisma"
 import EditPhoto from "@/components/editPhoto"
 const logger = require('@/components/utils/logger')
 import Layout from "@/components/layout/layout"
+import Link from "next/link"
+import GoBackButton from "@/components/utils/goBackButton"
 
 
 export default function ViewImage(props) {
@@ -20,7 +22,7 @@ export default function ViewImage(props) {
         categories,
         collections,
     } = props
-
+    console.log(photoCopies)
     const [selectedOption, setSelectedOption] = useState(null);
     const [priceOption, setPriceOption] = useState({
         size: null,
@@ -55,7 +57,7 @@ export default function ViewImage(props) {
             filename: photo.filename,
             thumbnail: thumbNailPhoto[0].filepath
         }
-    
+
         const result = await fetch('/api/cart/storeCartData', {
             method: 'POST',
             headers: {
@@ -92,7 +94,7 @@ export default function ViewImage(props) {
     return (
         <Layout>
             <div className="bg-custom-grey">
-                <button  className="inline-flex items-center m-4 px-4 py-2 border border-transparent text-base font-medium rounded-md text-black bg-white hover:bg-slate-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500" onClick={() => router.back()}>Go back</button>
+            <GoBackButton/>
                 <div className="flex flex-col justify-center mt-12 mx-auto px-4 sm:px-6 md:px-8 max-w-screen-xl">
                     <h1 className="text-white text-3xl text-center font-bold mt-8 mb-6">{photo.title}</h1>
 
@@ -124,45 +126,32 @@ export default function ViewImage(props) {
                                     <div className="flex justify-between items-start border-b-2 px-4 py-3 mb-3" key={index}>
                                         <span className={`flex gap-4 items-center ${copy.size == "original" && !photo.exclusive ? 'hidden' : ''}`}>
                                             <input type={"radio"} value={copy.price} onChange={() => choosePriceOption(copy)} name="priceChoice" className="focus:ring focus:ring-custom-grey-light mt-2" />
-                                            {copy.size != "original" ?
-                                                <p className="text-gray-600 whitespace-nowrap overflow-ellipsis overflow-hidden max-w-xs">{copy.size}</p>
-                                                :
-                                                <div>
-                                                    <p className="text-gray-600 font-semibold">Exclusive</p>
-                                                    <p className="text-gray-500 text-sm mt-2">Safeguard your artistic endeavors – this image will be excluded from our platform for the duration you require.</p>
-                                                </div>
-                                            }
+                                            <p className="text-gray-600 whitespace-nowrap overflow-ellipsis overflow-hidden max-w-xs">{copy.size}</p>
+                                            <div className={`${priceOption.size == copy.size ? "block" : "hidden"}`}>
+                                                <p className="text-gray-600 font-semibold"></p>
+                                                <p className="text-gray-500 text-sm mt-2">{copy.width}px/{copy.height}px   -{copy.filetype}</p>
+                                            </div>
+
                                         </span>
                                         <div className="flex items-center max-w-xs">
-                                            {copy.size != "original" ?
-                                                <p className="text-xl font-semibold text-gray-800 whitespace-nowrap overflow-ellipsis overflow-hidden mt-2">{formatCurrency(copy.price)}</p>
-                                                :
-                                                <div className={`flex items-center justify-end space-x-2`}>
-
-                                                </div>
-                                            }
+                                            <p className="text-xl font-semibold text-gray-800 whitespace-nowrap overflow-ellipsis overflow-hidden mt-2">{formatCurrency(copy.price)}</p>
                                         </div>
                                     </div>
                                 ))}
 
+                                <button
+                                    className="w-full py-2 px-4 mt-6 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 transition-all duration-300"
+                                    onClick={() => handleAddToCart(photo.id)}
+                                >
+                                    Add to cart
+                                </button>
 
-                                {selectedOption !== "original" ? (
-                                    <button
-                                        className="w-full py-2 px-4 mt-6 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700 transition-all duration-300"
-                                        onClick={() => handleAddToCart(photo.id)}
-                                    >
-                                        Add to cart
-                                    </button>
-                                ) : (
-                                    <button
-                                        className="w-full py-2 px-4 mt-6 font-semibold rounded-lg shadow-md text-white bg-blue-500 hover:bg-blue-700 transition-all duration-300"
-                                        onClick={() => handleMarketFreeze(photo.id)}
-                                    //TODO
-                                    //Add redirect to a contact form with the image information(id or something)
-                                    >
-                                        Contact Us
-                                    </button>
-                                )}
+                                <div className="mt-4">
+                                    <Link className="text-indigo-600 hover:text-indigo-800 font-semibold underline" href="/table">
+                                        Size guide
+                                    </Link>
+                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -177,11 +166,11 @@ export async function getServerSideProps(context) {
     const { img, folderpath } = context.query;
     const session = await getSession(context);
 
-    
+
     let props = {};
     let photo;
-//TODO
-//return only the collections connected to the photographer
+    //TODO
+    //return only the collections connected to the photographer
     try {
         const collections = await prisma.collection.findMany({
             select: {
@@ -218,7 +207,7 @@ export async function getServerSideProps(context) {
 
         const userEmail = session && session.user ? session.user.email : null;
 
-        props = { img, photo, session: userEmail, collections, categories, photoCopies};
+        props = { img, photo, session: userEmail, collections, categories, photoCopies };
 
         if (photo && session) {
             const photographer = await prisma.photographer.findUnique({
@@ -228,8 +217,8 @@ export async function getServerSideProps(context) {
             if (photographer) {
                 props.photographer = photographer;
             }
-            if(photographer.role != "admin"){
-                const filterCollections = collections.filter(col => { return col.photographerPersonID == photographer.personID})
+            if (photographer.role != "admin") {
+                const filterCollections = collections.filter(col => { return col.photographerPersonID == photographer.personID })
                 props.collections = filterCollections
             }
         }
