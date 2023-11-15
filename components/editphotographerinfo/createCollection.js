a0ca5d9503cb2d16be6f1132356a432cc0ae529a
Joakim <joakim@spprod.se>
2023-11-15 16:21:50 +0100

    UI for creating collection by photographer

---
+++ components/editphotographerinfo/createCollection.js
@@ -0,0 +1,134 @@
+import InputField from "../../components/utils/inputField";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import ErrorBoundary from "../../components/errorBoundery";
+import { logErrorToApi } from "../../components/utils/logErrorToApi";
+import Updatecollection from "./updateCollection";
+
+export default function AddNewCollection({ collections, photographer }) {
+    const [currentCol, setCurrentCol] = useState(collections);
+    const [imageUpload, setImageUpload] = useState();
+    const [activeView, setActiveView] = useState();
+
+    function setImage(e) {
+        setImageUpload(e.target.files[0]);
+
+    }
+
+    async function handleCreateNewCollections(e) {
+        e.preventDefault();
+        if (imageUpload.type == "image/tiff") return toast.error("Wrong image type");
+        let isUnique = true;
+
+        currentCol.map((item) => {
+            if (item.name === e.target.name.value) {
+                isUnique = false;
+            }
+        });
+
+        if (!isUnique) {
+            return toast.error("A collection with that name already exists");
+        }
+
+        const formData = new FormData();
+        formData.append('name', e.target.name.value);
+        formData.append('description', e.target.description.value);
+        formData.append('image', e.target.collection_avatar.files[0]);
+        formData.append('photographerID', photographer.personID);
+        formData.append('subtitle', e.target.subtitle.value);
+
+        try {
+            const response = await fetch("/api/application/collections/addCollection", {
+                method: "POST",
+                body: formData,
+            });
+
+            if (response.ok) {
+                toast("Created new Collection");
+                setCurrentCol((prev) => [...prev, response.data]);
+                window.location.reload();
+            } else {
+                toast.warn("Could not add collection");
+            }
+        } catch (error) {
+            console.log(error);
+            logErrorToApi({
+                message: error.message,
+                stack: error.stack
+            });
+        }
+    }
+
+    const handleDeleteCollections = async (e) => {
+        e.preventDefault();
+
+        if (window.confirm("Are you sure you want to delete this collection?")) {
+            setCurrentCol(prev => prev.filter(col => col.id !== e.currentTarget.value));
+
+            const response = await fetch("/api/application/collections/deleteCollection", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ id: e.currentTarget.value }),
+            });
+
+            if (response.ok) {
+                toast.warn("deleted collection");
+                window.location.reload();
+            } else {
+                toast.error("Could not delete collection");
+            }
+        }
+    }
+
+    const handleEditCollectionView = (e) => {
+        setActiveView(e.currentTarget.id);
+    }
+
+    return (
+        <div className={`relative`}>
+            {activeView && (
+                <div className={`absolute w-full h-full flex justify-center items-center z-10 transform transition-all duration-500 ease-out ${activeView ? "backdrop-blur-sm opacity-100 scale-100" : "opacity-0 scale-0"}`}>
+                    <Updatecollection collections={collections} setActiveView={setActiveView} id={activeView} photographers={photographer} />
+                </div>
+            )}
+            <ErrorBoundary>
+                <div className="text-center py-8 w-full">
+                    <h1 className="text-5xl text-white font-bold">Create New Collection</h1>
+                </div>
+
+                <div className="flex justify-between gap-8 w-full mb-8">
+                    <form onSubmit={handleCreateNewCollections} method="post" encType="multipart/form-data" className="flex-1 bg-white shadow-xl rounded-xl px-10 py-8">
+                        <InputField label="name" type="text" name="name" textColor={"black"} />
+                        <InputField label="description" type="text" name="description" textColor={"black"} />
+                        <InputField label="subtitle" type="text" name="subtitle" textColor={"black"} />
+                        <h4 className="block mb-2 text-gray-700 font-medium">Hero image/Thumbnail photo</h4>
+                        <p className="text-sm text-blue-600 mt-2 mb-">Please upload images in either JPG or PNG format.</p>
+                        <input onChange={setImage} className="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-white dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" accept="image/png, image/jpeg" type="file" name="collection_avatar" title="Choose Hero image for collection" />
+
+                        <div className="mt-4 flex justify-center">
+                            <button className="py-2 px-6 bg-custom-grey text-white font-semibold rounded-md hover:bg-custom-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-custom-grey focus:ring-white" type="submit">
+                                Create
+                            </button>
+                        </div>
+                    </form>
+                    <div className="flex-1 bg-gray-800 shadow-xl rounded-xl px-10 py-6">
+                        <h2 className="text-3xl text-white font-bold mb-6">Current collections you&apos;ve created</h2>
+                        <ul>
+                            {collections.map((col) => (
+                                <li key={col.id} className="flex justify-between my-4">
+                                    <span className="text-white">{col.name}</span>
+                                    <div>
+                                        <button id={col.id} onClick={handleEditCollectionView} className="text-yellow-300 hover:text-yellow-500 mr-4">Edit</button>
+                                        <button value={col.id} onClick={handleDeleteCollections} className="text-red-500 hover:text-red-700">Delete</button>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </ErrorBoundary>
+        </div>
+    );
+}
---
+++ components/editphotographerinfo/updateCollection.js
@@ -0,0 +1,144 @@
+import InputField from "../utils/inputField";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { logErrorToApi } from "../utils/logErrorToApi";
+import Router from "next/router";
+
+export default function Updatecollection({ collections, setActiveView, id, photographer }) {
+    const [imageUpload, setImageUpload] = useState();
+    const [formData, setFormData] = useState({
+        name: "",
+        description: "",
+        subtitle: "",
+        photographerID: "",
+        id: "",
+        previusImageFolder: "",
+    });
+    const currentCol = collections.find((col) => col.id == id);
+
+    console.log(currentCol)
+    
+
+    useEffect(() => {
+        if (currentCol) {
+            setFormData({
+                name: currentCol.name,
+                description: currentCol.description,
+                subtitle: currentCol.subtitle,
+                photographerID: currentCol.photographerPersonID,
+                id: currentCol.id,
+                previusImageFolder: currentCol.imagepathfolder,
+            });
+        }
+    }, [currentCol]);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleUpdateCollection = async (e) => {
+        e.preventDefault();
+
+        try {
+            const postData = new FormData();
+            Object.keys(formData).forEach(key => {
+                postData.append(key, formData[key]);
+            });
+            if (imageUpload) {
+                postData.append('image', imageUpload);
+            }
+
+            const response = await fetch("/api/application/collections/editCollection", {
+                method: "POST",
+                body: postData,
+            });
+
+            if (response.ok) {
+                toast("Updated Collection");
+                Router.reload()
+            } else {
+                toast.warn("Could not update collection");
+            }
+
+        } catch (error) {
+            console.log(error);
+            logErrorToApi({
+                message: error.message,
+                stack: error.stack
+            });
+        }
+    };
+
+    const renderView = () => {
+        for (let col of collections) {
+            if (col.id === id) {
+                return (
+
+                    <>
+                        <div className="flex-1">
+                            <h3 className="text-center text-2xl">{col.name}</h3>
+                        </div>
+                        <div className="flex-1">
+                            <form onSubmit={handleUpdateCollection} encType="multipart/form-data" method="post">
+                                <InputField label="name" type="text" name="name" value={formData.name} onChange={handleInputChange} required />
+                                <label htmlFor="description" className="block mb-2 text-gray-700 font-medium">description</label>
+                                <textarea className="border rounded-md p-2 w-full focus:ring focus:ring-custom-grey-light focus:border-transparent" type="textarea" name="description" value={formData.description} onChange={handleInputChange} required />
+                                <InputField label="subtitle" type="text" name="subtitle" value={formData.subtitle} onChange={handleInputChange} required />
+                                <p className="text-sm text-blue-600 mt-2 mb-4">Please upload images in either JPG or PNG format.</p>
+                                <input
+                                    onChange={(e) => { setImageUpload(e.target.files[0]) }}
+                                    className="block text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-white dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                    aria-describedby="collection_avatar_help"
+                                    id="collection_avatar"
+                                    type="file"
+                                    name="image"
+                                    accept="image/png, image/jpeg"
+                                />
+
+                                <div className="mt-4 flex justify-center">
+                                    <button className="py-2 px-6 bg-custom-grey text-white font-semibold rounded-md hover:bg-opacity-90 transition-all duration-300">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </>
+                )
+            }
+        }
+        return null;
+    };
+
+    return (
+        <div className="p-8 bg-white rounded-md border-4 z-10">
+            <div className="flex-1 flex justify-end">
+                <button
+                    type="button"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    onClick={() => setActiveView()}
+                >
+                    <svg
+                        className="w-3 h-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 14"
+                    >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                        />
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                </button>
+
+            </div>
+            {renderView()}
+        </div>
+    )
+}
