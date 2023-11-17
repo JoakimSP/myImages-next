2e0488868e99fe8dda4d5156d65b04b03e8e39f1
Joakim <joakim@spprod.se>
2023-11-17 09:20:00 +0100

    Refactor image validation

---
+++ components/utils/validateImageUpload.js
@@ -0,0 +1,28 @@
+export function validateImage (file, formData)  {
+    const MIN_SIZE_BYTES = 118 * 1048576; // 118 MB in bytes
+
+    return new Promise((resolve, reject) => {
+        if (!file) {
+            reject(new Error("Select an image"));
+            return;
+        }
+
+        // Ensure all form fields are filled out
+        if (!formData.title || !formData.description || !formData.priceSmall || !formData.priceMedium || !formData.priceLarge) {
+            reject(new Error("Please fill in all required fields."));
+            return;
+        }
+        // Check file type
+        if (file.type !== "image/tiff") {
+            reject(new Error("Only .tiff files are allowed."));
+            return;
+        }
+
+        // Check file size
+        if (file.size < MIN_SIZE_BYTES) {
+            reject(new Error("File size is too small, must be at least 118 MB."));
+            return;
+        }
+        resolve()
+    });
+};
