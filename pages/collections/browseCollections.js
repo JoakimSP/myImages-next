8e00ca56270e04cd300aa29c6c2ad91a8dd422c5
Joakim <joakim@spprod.se>
2023-11-23 15:06:42 +0100

    Update title text

--- pages/collections/browseCollections.js
+++ pages/collections/browseCollections.js
@@ -7,8 +7,8 @@ export default function BrowseCollections({ collections }) {
         <Layout>
             <div className="my-16">
                 <div className="my-16">
-                    <h1 className="text-white text-7xl text-center font-light mb-4">Browse Collections</h1>
-                    <h3 className="text-white text-3xl px-4 lg:px-36 text-center mb-4">Here you&apos;ll find all of our available Collections where we have gathered images in the same style and theme in pre selected Collections folders.</h3>
+                    <h1 className="text-white text-7xl text-center font-light mb-4">Browse our Collections</h1>
+                    <h3 className="text-white text-3xl px-4 lg:px-36 text-center mb-4">Here you&apos;ll find all of our available Collections where we have gathered images in the same style and theme in pre selected Collection folders.</h3>
                 </div>
                 <ShowAllCollection collections={collections} />
             </div>
