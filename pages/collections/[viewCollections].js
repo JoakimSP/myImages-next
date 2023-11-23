2d59df0b29a695541a20b1565b9e88e2e3da0d12
Joakim <joakim@spprod.se>
2023-11-23 15:07:24 +0100

    Update collection information UI

--- pages/collections/[viewCollections].js
+++ pages/collections/[viewCollections].js
@@ -28,53 +28,59 @@ export default function ViewCollections({ collection }) {
         <Layout>
             <div className='w-full h-auto min-h-screen' >
                 {/* Collection Header Image and Name */}
-                    <div className="min-h-[600px] man-h-[800px]">
-                        <div className='relative w-full h-[66vh]'>
-                            <Image src={`/api/images/viewImage?name=${collection.imagepath}`} alt={collection.name} fill={true} className={"object-cover"} />
-                            <div className='flex justify-center items-center absolute text-3xl md:text-9xl z-10 text-white w-full h-full'>
-                                <h1 className='mb-44 text-center'>{collection.name}</h1>
-                            </div>
+                <div className="min-h-[600px] man-h-[800px]">
+                    <div className='relative w-full h-[66vh]'>
+                        <Image src={`/api/images/viewImage?name=${collection.imagepath}`} alt={collection.name} fill={true} className={"object-cover"} />
+                        <div className='flex justify-center items-center absolute text-3xl md:text-9xl z-10 text-white w-full h-full'>
+                            <h1 className='mb-44 text-center'>{collection.name}</h1>
                         </div>
-                    </div>
-                    {/* Collection Description and First Photo */}
-                    <div className='flex flex-col md:flex-row'>
-                        <div className="flex-1 h-96 bg-custom-grey text-white text-left p-4 overflow-hidden">
-                            <div className='p-2 md:p-12 overflow-hidden'>
-                                <h2 className='text-3xl md:text-6xl'>{collection.name}</h2>
-                                <p className='text-xl mb-8'>{collection.subtitle}</p>
-                                <p className='text-lg whitespace-pre-wrap'>{collection.description}</p>
-                            </div>
-                        </div>
-                        {firstPhotoThumb && (
-                            <div className="flex-1 h-96 bg-custom-grey text-white pt-4 pr-4 md:block hidden">
-                                <div className="relative w-full h-full">
-                                    <Link href={`/images/viewimage?img=${encodeURIComponent(firstPhotoSmallWm?.filepath)}&folderpath=${firstPhotoSmallWm?.folderpath}`}>
-                                        <Image src={`/api/images/viewImage?name=${firstPhotoThumb.filepath}`} alt={collection.name} fill={true} className={"object-cover"} quality={100}/>
-                                    </Link>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                    {/* Collection Photos Grid */}
-                    <div className='w-full p-5 pb-10 mx-auto mb-10 gap-5 columns-1 md:columns-2 lg:columns-3 space-y-5 bg-custom-grey'>
-                        {Object.entries(groupedPhotos).map(([key, photoGroup], index) => {
-                            if (index === 0) return null; // Skip the first photo, already displayed
-                            const thumbPhoto = photoGroup.find(p => p.size === 'thumb');
-                            const smallWmPhoto = photoGroup.find(p => p.size === 'small-wm');
-                            return (
-                                <Link key={key} href={`/images/viewimage?img=${encodeURIComponent(smallWmPhoto?.filepath)}&folderpath=${smallWmPhoto?.folderpath}`}>
-                                        <Image
-                                            src={`/api/images/viewImage?name=${thumbPhoto?.filepath}`}
-                                            alt={thumbPhoto?.title || 'image'}
-                                            width={thumbPhoto?.width}
-                                            height={thumbPhoto?.height}
-                                            className="mb-5"
-                                            layout='responsive' />
-                                </Link>
-                            );
-                        })}
                     </div>
                 </div>
+                {/* Collection Description and First Photo */}
+                <div className='flex flex-col md:flex-row h-auto justify-center items-center py-28 px-4'>
+                    <div className="flex-1 bg-custom-grey text-white text-left overflow-hidden flex flex-col justify-center items-center">
+                        <div className='overflow-hidden max-w-[600px] text-center md:text-start '>
+                            <h2 className='text-3xl md:text-6xl'>{collection.name}</h2>
+                            <p className='text-xl mb-8'>{collection.subtitle}</p>
+                            <p className='text-lg whitespace-pre-wrap'>{collection.description}</p>
+                        </div>
+                    </div>
+                    {firstPhotoThumb && (
+                        <div className={`flex-1 bg-custom-grey text-white md:block`}>
+                            <div className="relative w-full h-full flex justify-center items-center">
+                                <Link href={`/images/viewimage?img=${encodeURIComponent(firstPhotoSmallWm?.filepath)}&folderpath=${firstPhotoSmallWm?.folderpath}`}>
+                                    <Image
+                                        src={`/api/images/viewImage?name=${firstPhotoThumb.filepath}`}
+                                        alt={collection.name}
+                                        width={firstPhotoThumb.width}
+                                        height={firstPhotoThumb.height}
+                                        className={"object-cover"}
+                                        quality={100} />
+                                </Link>
+                            </div>
+                        </div>
+                    )}
+                </div>
+                {/* Collection Photos Grid */}
+                <div className='w-full px-5 pb-10 mx-auto mb-10 gap-5 columns-1 md:columns-2 lg:columns-3 space-y-5 bg-custom-grey'>
+                    {Object.entries(groupedPhotos).map(([key, photoGroup], index) => {
+                        if (index === 0) return null; // Skip the first photo, already displayed
+                        const thumbPhoto = photoGroup.find(p => p.size === 'thumb');
+                        const smallWmPhoto = photoGroup.find(p => p.size === 'small-wm');
+                        return (
+                            <Link key={key} href={`/images/viewimage?img=${encodeURIComponent(smallWmPhoto?.filepath)}&folderpath=${smallWmPhoto?.folderpath}`}>
+                                <Image
+                                    src={`/api/images/viewImage?name=${thumbPhoto?.filepath}`}
+                                    alt={thumbPhoto?.title || 'image'}
+                                    width={thumbPhoto?.width}
+                                    height={thumbPhoto?.height}
+                                    className="mb-5"
+                                    layout='responsive' />
+                            </Link>
+                        );
+                    })}
+                </div>
+            </div>
         </Layout>
     )
 }
