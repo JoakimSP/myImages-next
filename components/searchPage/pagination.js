9af53b35c2c28c84f426f019aab994c346131516
Joakim <joakim@spprod.se>
2023-12-07 09:16:25 +0100

    Add flex wrap to pagination

--- components/searchPage/pagination.js
+++ components/searchPage/pagination.js
@@ -57,7 +57,7 @@ export default function Pagination({ totalPages, currentPage }) {
     const pages = paginationRange();
 
     return (
-        <div className="flex justify-center items-center gap-2 mt-4">
+        <div className="flex flex-wrap justify-center items-center gap-2 mt-4">
             {currentPage > 1 && (
                 <button 
                     className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300"
