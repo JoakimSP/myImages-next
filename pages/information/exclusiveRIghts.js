e541fbd9379df96553aff91ca022640456469e4d
Joakim <joakim@spprod.se>
2023-12-11 13:52:12 +0100

    Update to dynamic content

--- pages/information/exclusiveRIghts.js
+++ pages/information/exclusiveRIghts.js
@@ -1,56 +1,62 @@
 import Layout from "@/components/layout/layout"
 import Link from "next/link"
 import Image from "next/image"
+import prisma from "@/components/prisma"
 
-export default function ExclusiveRights() {
+export default function ExclusiveRights({ pricingExclusiveData }) {
     return (
         <Layout>
             <div className="pt-4 text-white w-full h-full px-4 md:px-0 bg-custom-grey">
-                <div className="relative h-96">
+                <div className="relative h-full">
                     <Image
-                        src={"/appcontent/46BD907A-83B4-490F-80F0-54E4293B5169.JPG"}
+                        src={`/api/images/viewImage?name=${pricingExclusiveData.image}`}
                         alt="Exclusive right image"
                         fill={true}
-                        className="object-cover"/>
+                        className="object-cover" />
                     <div className="flex flex-col">
                         <h1 className="mb-5 text-center text-4xl font-bold md:text-6xl z-10">
-                            Exclusive Photography: Own a Moment in Time
+                            {pricingExclusiveData.title}
                         </h1>
-                        <div className="mx-auto mb-6 max-w-4xl text-center leading-7 md:text-lg z-10">
-                            Picture this: An image so distinct, it exists only for you. Every photograph tells a story, but when you purchase with us, that story is exclusively yours. Dive into our curated collection and discover photos that don&apos;t just capture a moment, but become solely a part of your narrative.
+                        <div className="mx-auto mb-6 max-w-4xl text-center leading-7 md:text-lg z-10 whitespace-pre-wrap">
+                           {pricingExclusiveData.subtitle}
                         </div>
-                        <div className="mb-8 flex justify-center space-x-4 whitespace-nowrap z-10">
+                        <div className="mb-8 flex flex-col md:flex-row items-center justify-center whitespace-nowrap z-10">
                             <Link href="/information/howToAndPricing" className="z-10 px-2 py-2.5 text-xl font-bold hover:border-b-4 hover:text-blue-400  md:px-5">
                                 Photos & Sizes
                             </Link>
                             <Link href="/information/exclusiveRights" className="z-10 border-b-4 border-white px-2 py-2.5 text-xl font-bold text-gray-300 hover:border-b-4 hover:text-blue-400 md:px-5">
-                                How to use
+                            Exclusive Collection, how it works
                             </Link>
                         </div>
                     </div>
                 </div>
 
 
-                <div className="mx-auto mb-6 max-w-4xl text-center leading-7 md:text-lg">
-                    <ul className="list-decimal list-inside pl-5">
-                        <li>Absolute Originality: Every image sold is removed from our gallery forever. You won&apos;t find it anywhere else, ensuring that the artwork you purchase remains one-of-a-kind.</li>
-                        <li>Premium Quality: Our professional photographers are masters of their craft, capturing moments in the highest resolution and perfect clarity.</li>
-                        <li>Legally Protected: With our exclusive rights agreement, rest assured that the image you purchase is legally protected, ensuring its uniqueness remains intact.</li>
-                        <li>Diverse Collection: From breathtaking landscapes and intimate portraits to the abstract and avant-garde, our gallery spans diverse themes and subjects.</li>
-                        <li>Ready for Any Application: Whether it&apos;s for a major advertising campaign, the cover of your next bestseller, or a centerpiece in your living room, our images are versatile and ready to shine.</li>
-                    </ul>
-                    <p className="mt-5">
-                        Our mission is more than just selling photographs; it&apos;s about elevating your projects, brand, or spaces to a level of unmatched distinction. When you choose an exclusive photo from our collection, you&apos;re not just buying an image; you&apos;re investing in a vision, a sentiment, and a promise of unparalleled originality.
-                    </p>
-                    <p className="mt-5 font-bold">
-                        Our collection is ever-evolving, and once a photo is sold, it&apos;s gone forever. Don&apos;t miss the opportunity to be the sole owner of a moment in time. Browse our gallery and secure your exclusive image now.
-                    </p>
+                <div className="mx-auto mb-6 max-w-4xl text-center leading-7 md:text-lg whitespace-pre-wrap">
+                    {pricingExclusiveData.text}
                 </div>
-                <div className="text-center text-sm text-gray-400 mt-8">
-                    <p>Embrace the extraordinary, because some moments are too precious to be shared.</p>
-                    <br />
                 </div>
-            </div>
         </Layout>
     )
+}
+
+
+export async function getServerSideProps() {
+    const pricingExclusiveData = await prisma.pricingpageExclusive.findFirst({
+        where: {
+            id: "1"
+        },
+        select: {
+            title: true,
+            subtitle: true,
+            image: true,
+            text: true,
+        }
+    });
+
+    return {
+        props: {
+            pricingExclusiveData
+        }
+    };
 }
