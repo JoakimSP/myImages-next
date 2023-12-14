97d69dc9ba5eb05ea8ded7d601a8a168e64b96a9
Joakim <joakim@spprod.se>
2023-12-14 10:45:38 +0100

    Add API to create legalnotice

---
+++ pages/api/application/createLegalText.js
@@ -0,0 +1,25 @@
+import prisma from "@/components/prisma"
+import logger from "@/components/utils/logger"
+
+export default async function handler(req, res) {
+
+     try {
+       await prisma.legalnotice.update({
+        where: {
+            id: "1"
+        },
+        data : {
+            text: req.body
+        }
+       })
+
+       res.status(200).json({ message: 'New text added' })
+    } catch (error) {
+        logger.log('error', {
+            message: error.message,
+            stack: error.stack
+        })
+        res.status(500).json({Error: error})
+    } 
+    
+}
