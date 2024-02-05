15b0762ce76dbb671214d0a1f6808980a20628b8
Joakim <joakim@spprod.se>
2024-02-05 12:16:34 +0100

    Add placeholder svg to featured col

--- components/collections/showFeaturedCollection.js
+++ components/collections/showFeaturedCollection.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+
+
 export default function ShowFeaturedCollection({ featuredcol }) {
     const [activeGroup, setActiveGroup] = useState(0);
     const [slidesPerGroup, setSlidesPerGroup] = useState(4); // Start with default of 4
@@ -13,7 +15,11 @@ export default function ShowFeaturedCollection({ featuredcol }) {
         sortOrder: col.sortOrder
     }));
     const totalGroups = Math.ceil(slides.length / slidesPerGroup);
-    const sortedSlides = slides.sort((a, b) => {return a.sortOrder - b.sortOrder})
+    const sortedSlides = slides.sort((a, b) => { return a.sortOrder - b.sortOrder })
+    const svg = `<svg class="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 20 18" className="animate-pulse">
+    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+</svg>`;
+    const blurDataURL = `data:image/svg+xml;base64,${Buffer.from(svg).toString('base64')}`;
 
 
     useEffect(() => {
@@ -67,6 +73,8 @@ export default function ShowFeaturedCollection({ featuredcol }) {
                                     fill={true}
                                     sizes={"50vw, 25vw"}
                                     className="object-cover w-full"
+                                    placeholder="blur"
+                                    blurDataURL={blurDataURL}
                                 />
                             </Link>
                         </div>
@@ -112,3 +120,4 @@ export default function ShowFeaturedCollection({ featuredcol }) {
         </div>
     );
 }
+
