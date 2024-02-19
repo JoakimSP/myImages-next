cbe91433d92e8711879af5eec1594c23dc829472
Joakim <joakim@spprod.se>
2024-02-19 13:05:50 +0100

    Update forgot password UI

--- pages/auth/photographerSignIn.js
+++ pages/auth/photographerSignIn.js
@@ -42,9 +42,9 @@ export default function PhotographerSignIn() {
                     />
                 </div>
                 <div className="flex-1 flex flex-col justify-center mx-6 w-5/12 z-10">
-                <div className="flex-1">
-                            <GoBackButton />
-                        </div>
+                    <div className="flex-1">
+                        <GoBackButton />
+                    </div>
                     <form className="flex-[2_2_0%]">
                         <h2 className="my-4">Are you a photographer with specific credentials to this application?</h2>
                         {/* Photographer Login Heading */}
@@ -82,10 +82,15 @@ export default function PhotographerSignIn() {
                             </div>
                             {/* Remember me and Forgot password link */}
                             <div className="mb-6 flex items-center justify-between">
-                                <a href="#!" className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">
-                                    Forgot password?
-                                </a>
+                                <div>
+                                    <p className="text-sm text-gray-600 dark:text-gray-400">Forgot password? 
+                                        <Link href="/contact" className="text-blue-500 ml-2 hover:text-blue-600 dark:hover:text-blue-400">
+                                            Contact the admin to reset password
+                                        </Link>
+                                    </p>
+                                </div>
                             </div>
+
                             {/* Submit button */}
                             <button onClick={onSubmit} className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal bg-slate-50 text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
                                 Sign in
