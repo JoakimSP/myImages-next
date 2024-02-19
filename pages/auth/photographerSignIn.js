9f9868389740886376b1484362266c7a649f7569
Joakim <joakim@spprod.se>
2024-02-19 11:32:12 +0100

    Update photographer login page design

--- pages/auth/photographerSignIn.js
+++ pages/auth/photographerSignIn.js
@@ -29,58 +29,71 @@ export default function PhotographerSignIn() {
         }
     };
 
-  return (
-    <div className="h-screen w-full bg-custom-header-grey  text-white">
-        <GoBackButton/>
-        <div className="flex justify-center items-center pt-48 px-4">
-            <form className="">
-            <h2 className="my-4">Are you a photographer with specific credentials to this application?</h2>
-                                    {/* Photographer Login Heading */}
-                                        <div className="mb-6">
-                                            <h2 className="text-lg font-semibold dark:text-white">Photographer Login</h2>
-                                            <p className="text-sm text-gray-600 dark:text-gray-400">Use your email and password to sign in.</p>
-                                            {/* Email input */}
-                                            {errorMessage && <div className="text-red-500 mb-4">{errorMessage}</div>}
-                                            <div className="mb-6">
-                                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                                    Email Address
-                                                </label>
-                                                <input
-                                                    type="email"
-                                                    id="email"
-                                                    placeholder="Email address"
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                    onChange={(e) => (email.current = e.target.value)}
-                                                    required
-                                                />
-                                            </div>
-                                            {/* Password input */}
-                                            <div className="mb-6">
-                                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                                    Password
-                                                </label>
-                                                <input
-                                                    type="password"
-                                                    id="password"
-                                                    placeholder="Password"
-                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                    onChange={(e) => (pass.current = e.target.value)}
-                                                    required
-                                                />
-                                            </div>
-                                            {/* Remember me and Forgot password link */}
-                                            <div className="mb-6 flex items-center justify-between">
-                                                <a href="#!" className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">
-                                                    Forgot password?
-                                                </a>
-                                            </div>
-                                            {/* Submit button */}
-                                            <button onClick={onSubmit} className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal bg-slate-50 text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-                                                Sign in
-                                            </button>
-                                        </div>
-                                </form>
+    return (
+        <div className="h-screen w-full">
+            <div className="flex flex-1 h-full flex-wrap justify-center lg:justify-between">
+                {/* Left column container with background */}
+                <div className="absolute md:relative flex-2 mb-12 md:mb-0  h-full w-full md:w-8/12 ">
+                    <Image
+                        src="/appcontent/2CA85D8D-3960-41B4-98D6-4CAE42C68812.JPG"
+                        className=" object-cover"
+                        fill={true}
+                        alt="Phone image"
+                    />
+                </div>
+                <div className="flex-1 flex flex-col justify-center mx-6 w-5/12 z-10">
+                <div className="flex-1">
+                            <GoBackButton />
+                        </div>
+                    <form className="flex-[2_2_0%]">
+                        <h2 className="my-4">Are you a photographer with specific credentials to this application?</h2>
+                        {/* Photographer Login Heading */}
+                        <div className="mb-6">
+                            <h2 className="text-lg font-semibold dark:text-white">Photographer Login</h2>
+                            <p className="mb-3 text-sm text-gray-600 dark:text-gray-400">Use your email and password to sign in.</p>
+                            {/* Email input */}
+                            {errorMessage && <div className="text-red-500 mb-4">{errorMessage}</div>}
+                            <div className="mb-6">
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    Email Address
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    placeholder="Email address"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    onChange={(e) => (email.current = e.target.value)}
+                                    required
+                                />
+                            </div>
+                            {/* Password input */}
+                            <div className="mb-6">
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    placeholder="Password"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    onChange={(e) => (pass.current = e.target.value)}
+                                    required
+                                />
+                            </div>
+                            {/* Remember me and Forgot password link */}
+                            <div className="mb-6 flex items-center justify-between">
+                                <a href="#!" className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">
+                                    Forgot password?
+                                </a>
+                            </div>
+                            {/* Submit button */}
+                            <button onClick={onSubmit} className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal bg-slate-50 text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                                Sign in
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
