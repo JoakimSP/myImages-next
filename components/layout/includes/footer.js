c8a973a40c23a564a665eae8d8d20ced7f5ed133
Joakim <joakim@spprod.se>
2024-02-19 13:07:28 +0100

    Add more gap in footer

--- components/layout/includes/footer.js
+++ components/layout/includes/footer.js
@@ -11,7 +11,7 @@ function Footer() {
                 <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div className="md:flex md:justify-between">
             
-                        <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div className="grid grid-cols-2 gap-8 sm:gap-28 sm:grid-cols-3">
                             {/* <div>
                                 <h2 className="mb-6 text-sm font-semibold uppercase text-white">Resources</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
