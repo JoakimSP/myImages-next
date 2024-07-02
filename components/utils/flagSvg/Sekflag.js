08ba8a5be3bdff5e265d0bf0d9a5b7b19bf399d7
JoakimSP <joakim@spprod.se>
2024-07-02 08:00:13 +0200

    Add svg files

---
+++ components/utils/flagSvg/Sekflag.js
@@ -0,0 +1,9 @@
+const sekFlag = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5 mr-2">
+      <rect width="24" height="24" fill="#005B99"/>
+      <rect width="24" height="6" y="9" fill="#FFD700"/>
+      <rect width="6" height="24" x="9" fill="#FFD700"/>
+    </svg>
+  );
+
+module.exports = sekFlag
---
+++ components/utils/flagSvg/USflag.js
@@ -0,0 +1,12 @@
+const usdFlag = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-5 w-5 mr-2">
+      <rect width="24" height="24" fill="#B22234"/>
+      <path fill="#FFF" d="M0 2.1h24v2.1H0zM0 6.3h24v2.1H0zM0 10.5h24v2.1H0zM0 14.7h24v2.1H0zM0 18.9h24v2.1H0z"/>
+      <path fill="#3C3B6E" d="M0 0h10.2v7.4H0z"/>
+      <path fill="#FFF" d="M1.2 0.9l.3.9h.9l-.7.5.3.9-.7-.5-.7.5.3-.9-.7-.5h.9zM3.6 0.9l.3.9h.9l-.7.5.3.9-.7-.5-.7.5.3-.9-.7-.5h.9zM6 0.9l.3.9h.9l-.7.5.3.9-.7-.5-.7.5.3-.9-.7-.5h.9zM1.2 3.3l.3.9h.9l-.7.5.3.9-.7-.5-.7.5.3-.9-.7-.5h.9zM3.6 3.3l.3.9h.9l-.7.5.3.9-.7-.5-.7.5.3-.9-.7-.5h.9zM6 3.3l.3.9h.9l-.7.5.3.9-.7-.5-.7.5.3-.9-.7-.5h.9z"/>
+    </svg>
+  ); 
+
+
+
+module.exports = usdFlag
